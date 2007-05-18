06eb82fc99c370de47523f14ceb44d74dea533c3
vrabaud <vrabaud@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2007-05-18 02:21:14 +0000

    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@5419 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/playmovies.m
+++ images/playmovies.m
@@ -1,50 +1,56 @@
 % [4D] shows R videos simultaneously as a movie.
 %
+% USAGE
+%  playmovie( I, fps, loop )
+%
 % INPUTS
-%   I       - MxNxTxR or MxNx1xTxR or MxNx3xTxR array (if MxNxT calls playmovie)
-%   fps     - [optional] maximum number of frames to display per second
-%             use fps==0 to introduce no pause and have the movie play as fast as possible
-%   loop    - [optional] number of time to loop video (may be inf)
-%             if neg plays video forward then backward then forward etc.
+%  I       - MxNxTxR or MxNx1xTxR or MxNx3xTxR array (if MxNxT calls
+%            playmovie)
+%  fps     - [100] maximum number of frames to display per second use
+%            fps==0 to introduce no pause and have the movie play as
+%            fast as possible
+%  loop    - [0] number of time to loop video (may be inf),
+%            if neg plays video forward then backward then forward etc.
+%
+% OUTPUTS
 %
 % EXAMPLE
-%   load( 'images.mat' );
-%   playmovies( videos );
+%  load( 'images.mat' );
+%  playmovies( videos );
 %
 % DATESTAMP
-%   22-May-2006  5:00pm
+%  17-May-2007
 %
 % See also MONTAGES, PLAYMOVIE, MAKEMOVIES
 
-% Piotr's Image&Video Toolbox      Version 1.03   
-% Written and maintained by Piotr Dollar    pdollar-at-cs.ucsd.edu 
-% Please email me if you find bugs, or have suggestions or questions! 
- 
+% Piotr's Image&Video Toolbox      Version 1.03
+% Written and maintained by Piotr Dollar    pdollar-at-cs.ucsd.edu
+% Please email me if you find bugs, or have suggestions or questions!
+
 function playmovies( I, fps, loop )
-    if( nargin<2 || isempty(fps)) fps = 100; end;
-    if( nargin<3 || isempty(loop)) loop = 1; end;
 
-    nd=ndims(I); siz=size(I); nframes=siz(end-1);
-    if( nd==3 ) playmovie( I, fps, loop ); return; end;
-    if( iscell(I) ) error('cell arrays not supported.'); end;
-    if( ~(nd==4 || (nd==5 && any(size(I,3)==[1 3]))) )
-        error('unsupported dimension of I'); end;
-    inds={':'}; inds=inds(:,ones(1,nd-2));   
-    clim = [min(I(:)),max(I(:))];
+if( nargin<2 || isempty(fps)); fps = 100; end
+if( nargin<3 || isempty(loop)); loop = 1; end
 
-    h=gcf; colormap gray; figure(h); % bring to focus    
-    nplayed=0; 
-    while( nplayed < abs(loop) )
-        if( loop<0 && mod(nplayed,2)==1 )
-            order = nframes:-1:1;
-        else
-            order = 1:nframes;
-        end;
-        for i=order
-            geth=get(h); %figure was closed, cannot continue display
-            montage2(squeeze(I(inds{:},i,:)),1,[],clim); 
-            title(sprintf('frame %d of %d',i,nframes));
-            if(fps>0) pause(1/fps); else pause(eps); end;
-        end;
-        nplayed=nplayed+1;
-    end;
+nd=ndims(I); siz=size(I); nframes=siz(end-1);
+if( nd==3 ); playmovie( I, fps, loop ); return; end
+if( iscell(I) ); error('cell arrays not supported.'); end
+if( ~(nd==4 || (nd==5 && any(size(I,3)==[1 3]))) )
+  error('unsupported dimension of I'); end
+inds={':'}; inds=inds(:,ones(1,nd-2));
+clim = [min(I(:)),max(I(:))];
+
+h=gcf; colormap gray; figure(h); % bring to focus
+for nplayed = 1 : abs(loop)
+  if( loop<0 && mod(nplayed,2)==1 )
+    order = nframes:-1:1;
+  else
+    order = 1:nframes;
+  end;
+  for i=order
+    tic; try geth=get(h); catch return; end
+    montage2(squeeze(I(inds{:},i,:)),1,[],clim);
+    title(sprintf('frame %d of %d',i,nframes));
+    if(fps>0); pause(1/fps - toc); else pause(eps); end
+  end
+end
