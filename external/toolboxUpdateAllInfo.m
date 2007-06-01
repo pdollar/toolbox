87ddf184f6f63cb9d4de558430d678c2001e8361
vrabaud <vrabaud@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2007-06-01 23:01:33 +0000

    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@5560 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- external/toolboxUpdateAllInfo.m
+++ external/toolboxUpdateAllInfo.m
@@ -1,100 +1,69 @@
-function toolbox_updateallinfo
+function toolboxUpdateAllInfo
 
-toolbox_insertinfo('Contents.m');
-return 
+insertInfo('Contents.m');
 
-    % must start in /toolbox base directory
-    cd('classify/')
-    mfiles = dir('*.m'); 
-    for i=1:size(mfiles,1) toolbox_removeinfo(mfiles(i).name); end;
-    for i=1:size(mfiles,1) toolbox_insertinfo(mfiles(i).name); end;
+% must start in /toolbox base directory
+dirTot={ 'classify/', 'classify/private', 'filters', 'images', ...
+  'images/private', 'matlab' };
 
-    cd('private/')
-    mfiles = dir('*.m'); 
-    for i=1:size(mfiles,1) toolbox_removeinfo(mfiles(i).name); end;
-    for i=1:size(mfiles,1) toolbox_insertinfo(mfiles(i).name); end;
+for i=1:length(dirTot)
+  mfiles = dir([ dirTot{i}, '/*.m' ]);
+  for j=1:length(mfiles);
+    removeInfo(mfiles(j).name);
+    insertInfo(mfiles(j).name);
+  end
+end
 
-    cd('../../filters/')
-    mfiles = dir('*.m'); 
-    for i=1:size(mfiles,1) toolbox_removeinfo(mfiles(i).name); end;
-    for i=1:size(mfiles,1) toolbox_insertinfo(mfiles(i).name); end;
-
-    cd('../images/')
-    mfiles = dir('*.m'); 
-    for i=1:size(mfiles,1) toolbox_removeinfo(mfiles(i).name); end;
-    for i=1:size(mfiles,1) toolbox_insertinfo(mfiles(i).name); end;
-
-    cd('private/')
-    mfiles = dir('*.m'); 
-    for i=1:size(mfiles,1) toolbox_removeinfo(mfiles(i).name); end;
-    for i=1:size(mfiles,1) toolbox_insertinfo(mfiles(i).name); end;
-
-    cd('../../matlab/');
-    mfiles = dir('*.m'); 
-    for i=1:size(mfiles,1) toolbox_removeinfo(mfiles(i).name); end;
-    for i=1:size(mfiles,1) toolbox_insertinfo(mfiles(i).name); end;
-
-    cd('../');
-
-
-    
 % Inserts toolbox data after main comment in an .m file.
 % Does not affect files with no body (such as Contents.m)
-%
-% EXAMPLE
-%   mfiles = dir('*.m'); for i=1:size(mfiles,1) toolbox_insertinfo(mfiles(i).name); end;
-function toolbox_insertinfo( fname )
-    fid_in = fopen( fname, 'rt' );
-    fid_out = fopen( [fname '2'] , 'wt' );
+function insertInfo( fname )
 
-    insert_str = ...
-        ['%% Piotr''s Image&Video Toolbox      Version 1.03   \n' ...
-         '%% Written and maintained by Piotr Dollar    pdollar-at-cs.ucsd.edu \n' ...
-         '%% Please email me if you find bugs, or have suggestions or questions! \n \n' ];
-    
-    first_comment = true;
-    while 1
-        tline = fgetl(fid_in);
-        if ~ischar(tline), break, end;
-        fprintf( fid_out, '%s\n', tline );
-        if( length(strtrim(tline))==0 && first_comment )
-            first_comment = false;
-            fprintf( fid_out, insert_str );
-        end
-    end
-    
-    fclose(fid_in);
-    fclose(fid_out);
-    movefile( [fname '2'], [fname] );
-        
+fid_in = fopen( fname, 'rt' ); fid_out = fopen( [fname '2'] , 'wt' );
 
-    
+first_comment = true;
+while 1
+  tline = fgetl(fid_in);
+  if ~ischar(tline); break; end;
+  fprintf( fid_out, '%s\n', tline );
+  if( isempty(strtrim(tline)) && first_comment )
+    first_comment = false;
+    fprintf( fid_out, [...
+      '%% Piotr''s Image&Video Toolbox      Version 1.03   \n' ...
+      '%% Written and maintained by Piotr Dollar    pdollar-at-cs.ucsd.edu \n' ...
+      '%% Please email me if you find bugs, or have suggestions or questions! \n \n'] );
+  end
+end
+
+fclose(fid_in); fclose(fid_out);
+movefile( [fname '2'], fname );
+
 % Removes toolbox data after main comment in an .m file.
-%
-% EXAMPLE
-%   mfiles = dir('*.m'); for i=1:size(mfiles,1) toolbox_removeinfo(mfiles(i).name); end;
+function removeInfo( fname )
 
-function toolbox_removeinfo( fname )
-    fid_in = fopen( fname, 'rt' );
-    fid_out = fopen( [fname '2'] , 'wt' );
+% Read the first part of the comments
+fid = fopen( fname, 'rt' ); ind=0;
+while 1
+  ind = ind + 1;
+  fileLine{ind} = fgetl(fid);
+  if ~ischar(fileLine{ind}) || isempty(strtrim(fileLine{ind})); break; end
+end
 
-    nlines = 4; % number of lines to remove
-    removedinfo = -1;
-    while 1
-        tline = fgetl(fid_in);
-        if ~ischar(tline), break, end;
-        if( length(strtrim(tline))==0 && removedinfo==-1 )
-            fprintf( fid_out, '%s\n', tline );
-            removedinfo = 0;
-        elseif( removedinfo>=0 && removedinfo<nlines )
-            removedinfo = removedinfo+1;
-        else
-            fprintf( fid_out, '%s\n', tline );
-        end;
-    end
-    
-    fclose(fid_in);
-    fclose(fid_out);
-    movefile( [fname '2'], [fname] );
-        
-    
+if ind>1; if strfind(fileLine{ind-1},'Piotr'); ind=ind-1; end; end
+
+% Skip the second part of the comments
+while 1
+  fileLineTemp = fgetl(fid);
+  if ~ischar(fileLineTemp) || isempty(strtrim(fileLineTemp)); break; end
+end
+
+% Read the rest
+while 1
+  ind = ind + 1;
+  fileLine{ind} = fgetl(fid);
+  if ~ischar(fileLine{ind}); break; end
+end
+
+% Write the file
+fclose(fid); fid = fopen( fname, 'wt' );
+for i=1:ind; fprintf( fid, '%s\n', fileLine{i} ); end
+fclose(fid);
