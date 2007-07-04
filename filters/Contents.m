9ac16543070a35b2b4db368197c11b894804ac81
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2007-07-04 20:04:41 +0000

    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@5865 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- filters/Contents.m
+++ filters/Contents.m
@@ -11,7 +11,7 @@
 %   filter_gabor_2D     - Creates an even/odd pair of 2D Gabor filters.
 %   filter_gauss_1D     - 1D Gaussian filter.
 %   filter_gauss_nD     - n-dimensional Gaussian filter. 
-%   filter_steerable    - Steerable Gaussian derivative filter.
+%   filter_steerable    - Steerable 2D Gaussian derivative filter (for visualization).
 %   filter_visualize_1D - Used to visualize a 1D filter.
 %   filter_visualize_2D - Used to visualize a 2D filter.  
 %   filter_visualize_3D - Used to visualize a 3D filter. 
@@ -19,9 +19,9 @@
 % Operations involving a set of filters (a filter bank or FB):
 %   FB_apply_2D         - Applies each of the filters in the filterbank FB to the image I.
 %   FB_crop             - Crop a 2D filterbank (adjusting filter norms).
-%   FB_make_1D          - Various ways to make filterbanks.  See inside of this file for details.
-%   FB_make_2D          - Various ways to make filterbanks.  See inside of this file for details.
-%   FB_make_3D          - Various ways to make filterbanks.  See inside of this file for details.
+%   FB_make_1D          - Various 1D filterbanks (hardcoded).
+%   FB_make_2D          - Various 2D filterbanks (hardcoded).
+%   FB_make_3D          - Various 3D filterbanks (hardcoded).
 %   FB_reconstruct_2D   - Use to see how much image information is preserved in filter outputs.
 %   FB_visualize_1D     - Used to visualize the Fourier spectra of a series of 1D filters.  
 %   FB_visualize_2D     - Used to visualize the Fourier spectra of a series of 2D filters.
