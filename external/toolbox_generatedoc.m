c76ac15e6d8657d302fa3ae1fe8722df61356e2e
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2007-07-06 21:27:55 +0000

    -deleting old documentation
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@5917 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- external/toolbox_generatedoc.m
+++ external/toolbox_generatedoc.m
@@ -1,5 +1,17 @@
 % Generate documentation, must run from dir toolbox.
 %
+% Prior to running, update a few things in the overview.html file in:
+%   toolbox/external/m2html/templates/frame-piotr/overview.html
+%   1) The version / date
+%   2) Link to rar/zip file
+%
+% After running, remove links to Subsequent directories (*/private and
+% */.svn from):
+%  doc/classify/menu.html
+%  doc/iamges/menu.html
+%  doc/filters/menu.html
+%  doc/matlab/menu.html
+% 
 % USAGE
 %  toolbox_generatedoc
 %
@@ -15,18 +27,14 @@
 % Written and maintained by Piotr Dollar    pdollar-at-cs.ucsd.edu
 % Please email me if you find bugs, or have suggestions or questions!
 
-% The template frame-piotr is frame copied over, with a special start page
-% (overview.html), and Contents.html given a link from every menu.  Also,
-% after generating the documentation, removed links to */private from main
-% doc/menu.html. (Currently script simply copies over menu.html that is
-% fixed).
-
-% After, make sure to update 1) date and 2) link to zip file in
-% doc/Overview.html. Also history.txt get copied over
-
+% run m2html
 params = {'mfiles',{'classify','images','filters','matlab'}};
 params = {params{:},'htmldir','doc','recursive','on','source','off'};
 params = {params{:},'template','frame-piotr','index','menu','global','on'};
 m2html(params{:});
+
+% copy custom menu.html
 copyfile('external\m2html\templates\menu-for-frame-piotr.html','doc/menu.html')
+
+% copy history file
 copyfile('external\history.txt','doc/history.txt')
