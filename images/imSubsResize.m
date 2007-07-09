85aa59b78187cc05edbc1acb95d38ae7e0db0fd2
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2007-07-09 08:37:47 +0000

    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@6021 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/imSubsResize.m
+++ images/imSubsResize.m
@@ -1,25 +1,27 @@
+% Resizes subs by resizVals.
+%
 % Resizes subs in subs/vals image representation by resizVals.
 %
-% imsubs is a 'bag of pixels' image representation which is useful for
-% sparse arrays.
-%
-% This essentially repleces each sub by sub.*resizVals.  The only subtlety
+% This essentially replaces each sub by sub.*resizVals.  The only subtlety
 % is that in images the leftmost sub value is .5, so for example when
 % resizing by a factor of 2, the first pixel is replaced by 2 pixels and so
 % location 1 in the original image goes to location 1.5 in the second
 % image, NOT 2.  It may be necessary to round the values afterward.
 %
 % USAGE
-%  subs = imSubsResize( subs, resizVals )
+%  subs = imSubsResize( subs, resizVals, [zeroPnt] )
 %
 % INPUTS
 %  subs        - subscripts of point locations (n x d)
 %  resizVals   - k element vector of shrinking factors
+%  zeroPnt     - [.5] See comment above.
 %
 % OUTPUTS
 %  subs        - transformed subscripts of point locations (n x d)
 %
 % EXAMPLE
+%  subs = imSubsResize( [1 1; 2 2], [2 2] )
+%
 %
 % See also IMSUBSTOARRAY
 
@@ -27,11 +29,13 @@
 % Written and maintained by Piotr Dollar    pdollar-at-cs.ucsd.edu
 % Please email me if you find bugs, or have suggestions or questions!
 
-function subs = imSubsResize( subs, resizVals )
+function subs = imSubsResize( subs, resizVals, zeroPnt )
+
+if( nargin<3 || isempty(zeroPnt) ); zeroPnt=.5;  end
 
 [n d] = size(subs);
 [resizVals,er] = checkNumArgs( resizVals, [1 d], -1, 2 ); error(er);
 
 % transform subs
 resizVals = repmat( resizVals, [n, 1] );
-subs = (subs - .5) .* resizVals +.5;
+subs = (subs - zeroPnt) .* resizVals + zeroPnt;
