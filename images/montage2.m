6364c053fb1e565114ddf1c60382869303e56fe6
vrabaud <vrabaud@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2007-07-10 04:51:15 +0000

    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@6051 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/montage2.m
+++ images/montage2.m
@@ -9,20 +9,34 @@
 %
 % INPUTS
 %  IS          - MxNxT or MxNx1xT or MxNx3xT array (of bw or color images)
+%                MxNxTxR or MxNx1xTxR or MxNx3xTxR array, or cell array
+%                  where each element is MxNxT or MxNx1xT or MxNx3xT
 %  showLns     - [0] whether to show lines separating the various frames
 %  extraInf    - [0] if 1 then a colorbar is shown as well as pixval bar
 %  clim        - [] clim = [clow chigh] optional scaling of data
 %  m           - [] #images/col (if [] then calculated)
 %  nn          - [] #images/row (if [] then calculated)
-%  labels      - [] cell array of strings specifying labels for each image
+%  label       - [] cell array of strings specifying labels for each image
 %
 % OUTPUTS
 %  h           - image handle
 %  m           - #images/col
 %  nn          - #images/row
 %
-% EXAMPLE
-%  load( 'images.mat' ); montage2( images, 1 );
+% EXAMPLE - 1
+%
+%  load( 'images.mat' );
+%  imClusters = clustermontage( images, IDXi, 16, 1 );
+%  figure(1); montage2( imClusters, struct('perRow',1,'padSize',1) );
+%  figure(2); montage2( videos, struct('perRow',1));
+%
+% EXAMPLE - 2
+%
+%  load( 'images.mat' ); montage2( videos );
+%
+% EXAMPLE - 3
+%
+%  load( 'images.mat' ); montage2( images );
 %
 % See also MONTAGE, MAKEMOVIE
 
@@ -30,14 +44,103 @@
 % Written and maintained by Piotr Dollar    pdollar-at-cs.ucsd.edu
 % Please email me if you find bugs, or have suggestions or questions!
 
-function varargout=montage2( IS, showLns, extraInf, clim, mm, nn, labels )
+function varargout = montage2( IS, prm )
 
-if (nargin<2 || isempty(showLns)); showLns = 0; end;
-if (nargin<3 || isempty(extraInf)); extraInf = 0; end;
-if (nargin<4 || isempty(clim)); clim = []; end;
-if (nargin<5 || isempty(mm)); mm = []; end;
-if (nargin<6 || isempty(nn)); nn = []; end;
-if (nargin<7 || isempty(labels)); labels = {}; end;
+if nargin<2; prm=struct(); end
+
+%%% get parameters (set defaults)
+dfs = {'showLine',0,'extraInf',0,'clim',[],'mm',[],'nn',[],...
+  'label',[],'perRow',false,'padSize',4,'montageLabel',[]};
+prm = getPrmDflt( prm, dfs );
+extraInf=prm.extraInf;
+label=prm.label; perRow=prm.perRow; padSize=prm.padSize;
+montageLabel=prm.montageLabel;
+
+siz=size(IS); nd=ndims(IS);
+
+
+%%% Deal with the special way of doing a montage
+if perRow
+  [padSize,er] = checkNumArgs( padSize,[1 1], 0, 1 ); error(er);
+
+  if iscell(IS)
+    error('Invalid input, IS cannot be a cell');
+  end
+  if ~any(size(IS,3)==[1 3]);
+    reshape(IS,[siz(1),siz(2),1,siz(3:end)]);
+  else
+    error('Invalid input, IS has to be MxNxT or MxNx1xT or MxNx3xTxR');
+  end
+  % reshape IS so that each 3D element is concatenated to a 2D image,
+  % adding padding
+  padEl = max(IS(:));
+  size(IS)
+  IS=arrayCrop(IS, ones(1,nd), [siz(1)+padSize siz(2:end)], padEl ); %UD pad
+  siz=size(IS);
+  switch nd
+    case 4
+      IS=squeeze( reshape( IS, siz(1), [], siz(4) ) );
+    case 5
+      IS=squeeze(reshape(permute(IS,[1 2 4 3 5]),size(IS,1),[],...
+        size(IS,3),size(IS,5)));
+  end
+  siz=size(IS); nd=ndims(IS);
+  IS=arrayCrop(IS, [(1-padSize)*[1 1], ones(1,nd-2)], ...
+    [siz(1) siz(2)+padSize siz(3:end)], padEl);
+
+  % show using montage2
+  varargout = cell(1,nargout);
+  if( nargout); varargout{1}=IS; end
+  prm.perRow=false;
+  [varargout{2:end}] = subMontage( IS, prm );
+  title(inputname(1));
+  return
+end
+
+
+%%% Otherwise, display each montage
+siz=size(IS);
+if ~iscell(IS)
+  if ~any(size(IS,3)==[1 3]); IS=reshape(IS,[siz(1:2),1,siz(3:end)]); end
+  IS2=cell(1,size(IS,5));
+  for i=1:size(IS,5); IS2{i}=IS(:,:,:,:,i); end
+  IS=IS2; clear IS2;
+end
+
+nmontages = numel(IS);
+
+if numel(IS)>1; 
+  nn2 = ceil( sqrt(nmontages) ); mm2 = ceil(nmontages/nn2); 
+else
+  mm2=1; nn2=1;
+end
+
+% draw each montage
+for i=1:nmontages
+  subplot(mm2,nn2,i);
+
+  prm2=prm;
+  if ~isempty(montageLabel); prm2.label=prm.montageLabel{i}; end
+  if( ~isempty(IS{i}) )
+    subMontage( IS{i}, prm2 );
+  else
+    set(gca,'XTick',[]); set(gca,'YTick',[]);  %extraInf off
+  end
+  if(~isempty(label)); title(label{i}); end
+end
+if extraInf; pixval on; end
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function varargout=subMontage( IS, prm )
+
+dfs = {'showLine',0,'extraInf',0,'clim',[],'mm',[],'nn',[],...
+  'labels',[],'perRow',false,'padSize',0};
+
+prm = getPrmDflt( prm, dfs );  showLine=prm.showLine;
+extraInf=prm.extraInf;  clim=prm.clim; mm=prm.mm; nn=prm.nn;
+labels=prm.labels;
 
 % take care of single images
 if( ndims(IS)==2)
@@ -91,11 +194,11 @@ else
   set(gca,'XTick',[]); set(gca,'YTick',[]);
 end;
 
-% draw lines seperating frames
-if( showLns )
+% draw lines separating frames
+if( showLine )
   montageWd = nn * siz(2) + .5;  montageHt = mm * siz(1) + .5;
   for i=1:mm-1
-    height = i*siz(1)+.5; line([.5,montageWd],[height,height]); 
+    height = i*siz(1)+.5; line([.5,montageWd],[height,height]);
   end
   for i=1:nn-1
     width = i*siz(2)+.5; line([width,width],[.5,montageHt]);
@@ -129,4 +232,3 @@ end
 
 % optional output
 if( nargout>0 ); varargout={h,mm,nn}; end
-
