b6fad1f440d1f596c6bfd18daca914cf449b89e6
vrabaud <vrabaud@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2007-07-10 08:22:24 +0000

    - minor cleanup
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@6065 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/montage2.m
+++ images/montage2.m
@@ -43,7 +43,7 @@
 %
 % See also MONTAGE, PLAYMOVIE
 
-% Piotr's Image&Video Toolbox      Version 1.5
+% Piotr's Image&Video Toolbox      Version NEW
 % Written and maintained by Piotr Dollar    pdollar-at-cs.ucsd.edu
 % Please email me if you find bugs, or have suggestions or questions!
 
