5c7c169b3998fcc21506fc744861bbf818701644
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2007-10-08 23:44:22 +0000

    -made im.m compatible with Matlab2007b (by removing impixelinfo which was causing annoying problems)
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@6683 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/im.m
+++ images/im.m
@@ -17,9 +17,9 @@
 % EXAMPLE
 %  load clown; im( X )
 %
-% See also IMSHOW, IMVIEW, IMPIXELINFO
+% See also IMSHOW, IMVIEW, IMPIXELINFO, IMTOOL
 
-% Piotr's Image&Video Toolbox      Version 2.0
+% Piotr's Image&Video Toolbox      Version NEW
 % Copyright (C) 2007 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Liscensed under the Lesser GPL [see external/lgpl.txt]
@@ -38,9 +38,9 @@ colormap(gray);
 title(inputname(1));
 axis('image');
 
-% info about image pixels
+% info about image pixels - removed impixelinfo for 2007b
 if( extraInf )
-  impixelinfo;
+  %impixelinfo;
   colorbar;
 else
   set(gca,'XTick',[]);
