21f4cd30515a8bc9f5aed9b60d05c834834b4453
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2007-11-02 01:55:48 +0000

    -optimized images/imtransform2
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@6795 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/imtransform2.m
+++ images/imtransform2.m
@@ -34,10 +34,16 @@
 % OUTPUTS
 %  IR      - transformed image
 %
-% EXAMPLE - general homography
+% EXAMPLE - general homography (rotation + translation)
 %  load trees; I=X;
 %  R = rotationMatrix( pi/4 ); T = [1; 3]; H = [R T; 0 0 1];
 %  IR = imtransform2( I, H, [], 'crop', 1 );
+%
+% EXAMPLE - general homography (out of plane rotation)
+%  load trees; I=X;
+%  R = rotationMatrix( [0 1 0], pi/4 );  z=500;
+%  H = R; H(1:2,:)=H(1:2,:)*z; H(:,3)=H(:,3)*z;
+%  IR = imtransform2(I,H,'nearest','loose',1);
 %
 % EXAMPLE - rotation
 %  load trees;
@@ -49,11 +55,11 @@
 % EXAMPLE - translation
 %  load trees;
 %  XT = imtransform2(X,0,1.5,'bicubic','crop');
-%  figure(1); clf; im(X,[0 255]); figure(2); clf; im(XT,[0 255]);
+%  figure(1); clf; im(X,[0 128]); figure(2); clf; im(XT,[0 128]);
 %
 % See also TEXTUREMAP, INTERP2
 
-% Piotr's Image&Video Toolbox      Version 2.02
+% Piotr's Image&Video Toolbox      Version NEW
 % Copyright (C) 2007 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Liscensed under the Lesser GPL [see external/lgpl.txt]
@@ -81,52 +87,54 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function IR = imtransform2main( I, H, method, bbox, show )
 
-% General homography case
-if( ndims(I)~=2 ); error('I must a MxN array'); end
-if(any(size(H)~=[3 3])); error('H must be 3 by 3'); end
-if(rank(H)~=3); error('H must be full rank.'); end
-if(~isempty(bbox) && ~any(strcmp(bbox,{'loose','crop'})));
- error(['illegal value for bbox: ' bbox]); 
-end
-
+% check inputs
 if( nargin<3 || isempty(method)); method='linear'; end
 if( nargin<4 || isempty(bbox)); bbox='loose'; end
 if( nargin<5 || isempty(show)); show=0; end
-
-className = class( I );
-if(~strcmp(className,'double')); I = double(I); end
-I = padarray(I,[1,1],eps,'both');
-siz = size(I);
+if( ndims(I)~=2 ); error('I must a MxN array'); end
+if( any(size(H)~=[3 3])); error('H must be 3x3'); end
+if( rank(H)~=3); error('H must be full rank.'); end
+if( ~any(strcmp(bbox,{'loose','crop'})));
+ error(['illegal value for bbox: ' bbox]); 
+end
 
 % set origin to be center of image
-rStr = (-siz(1)+1)/2; rEnd = (siz(1)-1)/2;
-cStr = (-siz(2)+1)/2; cEnd = (siz(2)-1)/2;
+sizI = size(I)+2;
+rStr = (-sizI(1)+1)/2; rEnd = (sizI(1)-1)/2;
+cStr = (-sizI(2)+1)/2; cEnd = (sizI(2)-1)/2;
 
-% If 'bbox' then get bounds of resulting image. To do this project the
+% If 'loose' then get bounds of resulting image. To do this project the
 % original points accoring to the homography and see the bounds.  Note
 % that since a homography maps a quadrilateral to a quadrilateral only
 % need to look at where the bounds of the quadrilateral are mapped to.
 if( strcmp(bbox,'loose') )
-  pr = H * [rStr rEnd rStr rEnd; cStr cStr cEnd cEnd; 1 1 1 1];
-  rowDst = pr(1,:) ./ pr(3,:);  colDst = pr(2,:) ./ pr(3,:);
+  P = H * [rStr rEnd rStr rEnd; cStr cStr cEnd cEnd; 1 1 1 1];
+  rowDst = P(1,:)./P(3,:);  colDst=P(2,:)./P(3,:);
   rStr = min(rowDst(:));  rEnd = max(rowDst(:));
   cStr = min(colDst(:));  cEnd = max(colDst(:));
 end
 
 % apply inverse homography on meshgrid in destination image
-[colGrid,rowGrid] = meshgrid( cStr:cEnd, rStr:rEnd );
-[mRow,nCol] = size( colGrid );
-pr = inv(H) * [rowGrid(:)'; colGrid(:)'; ones(1,mRow*nCol)];
-rowLocs = pr(1,:) ./ pr(3,:) + (siz(1)+1)/2;
-rowLocs = reshape(rowLocs,mRow,nCol);
-colLocs = pr(2,:) ./ pr(3,:) + (siz(2)+1)/2;
-colLocs = reshape(colLocs,mRow,nCol);
+[colGr,rowGr] = meshgrid(cStr:cEnd,rStr:rEnd); sizIR=size(colGr);
+P = H \ [rowGr(:)'; colGr(:)'; ones(1,prod(sizIR))];
+rows = reshape( P(1,:)./P(3,:), sizIR ) + (sizI(1)+1)/2;
+cols = reshape( P(2,:)./P(3,:), sizIR ) + (sizI(2)+1)/2;
 
-% now texture map results
-IR = interp2( I, colLocs, rowLocs, method );
-IR(isnan(IR)) = 0;
-IR = arrayToDims( IR, size(IR)-2 ); %undo extra padding (exact if 'crop')
-if(~strcmp(className,'double')); IR=feval(className,IR ); end
+% now texture map results ('nearest' inlined for speed)
+classI = class( I );
+if(~strcmp(classI,'double')); I=double(I); end
+I = padarray(I,[1,1],eps,'both');
+if( strcmp(method,'nearest') )
+  rows=floor(rows+.5); rows=min( max(rows,1), sizI(1) );
+  cols=floor(cols+.5); cols=min( max(cols,1), sizI(2) );
+  locs = rows+(cols-1)*sizI(1);
+  IR = I( locs );
+else  
+  IR = interp2( I, cols, rows, method );
+  IR(isnan(IR)) = 0;
+end
+IR = arrayToDims( IR, sizIR-2 );
+if(~strcmp(classI,'double')); IR=feval(classI,IR ); end
 
 % optionally show
 if( show )
