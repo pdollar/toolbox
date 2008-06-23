c68f1c504276cd9c40e42b692dc1e4fab4db47b4
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2008-06-23 19:51:58 +0000

    -NEW HEADER: will shortly be converting entire toolbox to this format --comment moved below "function..." --toolbox info made as part of main comment
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@9640 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- external/toolboxHeader.m
+++ external/toolboxHeader.m
@@ -1,21 +1,21 @@
+function [y1,y2] = toolboxHeader( x1, x2, x3, prm )
 % One line description of function (will appear in file summary).
 %
 % General commments explaining purpose of function [width is 75
-% characters]. There may be multiple paragraphs.  In special cases some or
+% characters]. There may be multiple paragraphs. In special cases some or
 % all of these guidelines may need to be broken.
 %
 % Next come a series of sections, including USAGE, INPUTS, OUTPUTS,
-% EXAMPLE, and "See also".  Each of these fields should always appear, even
-% if nothing follows (for example no inputs).  USAGE should usually be a
+% EXAMPLE, and "See also". Each of these fields should always appear, even
+% if nothing follows (for example no inputs). USAGE should usually be a
 % copy of the first line of code (which begins with "function"), minus the
 % word "function". Optional parameters are surrounded by brackets.
 % Occasionally, there may be more than 1 distinct usage, in this case list
-% additional usages.  In general try to avoid this.  INPUTS/OUTPUTS are
+% additional usages. In general try to avoid this. INPUTS/OUTPUTS are
 % self explanatory, however if there are multiple usages can be subdivided
-% as below.  EXAMPLE should list 1 or more useful examples.  Main comment
-% should all appear as one contiguous block.  Next a space, and then a
-% short comment that includes the toolbox version.  All other comments
-% should appear after the function begins.
+% as below. EXAMPLE should list 1 or more useful examples. Main comment
+% should all appear as one contiguous block. Next a blank comment line,
+% and then a short comment that includes the toolbox version.
 %
 % USAGE
 %  xsum = toolboxHeader( x1, x2, [x3], [prm] )
@@ -26,9 +26,9 @@
 %  x2          - descr. of variable 2, keep spacing like this
 %                if descr. spans multiple lines do this
 %  x3          - [0] indicates an optional variable, put def val in []
-%  prm         - [] param struct (preferred over key/value list)
-%       .p1      parameter 1 descr.
-%       .p2      parameter 2 descr.
+%  prm         - [] param struct
+%       .p1      parameter 1 descr
+%       .p2      parameter 2 descr
 %
 % OUTPUTS - and whatever after the dash
 %  xsum        - sum of xs
@@ -43,20 +43,18 @@
 % EXAMPLE - example 2
 %  y = toolboxHeader( 2, 3 );
 %
-% See also FUNCTIONALLCAPS
-
-% Piotr's Image&Video Toolbox      Version 1.5
+% See also GETPRMDFLT
+%
+% Piotr's Image&Video Toolbox      Version NEW
 % Copyright (C) 2007 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Lesser GPL [see external/lgpl.txt]
 
-function [y1,y2] = toolboxHeader( x1, x2, x3, prm ) %#ok<INUSD>
+% optional arguments x3 and prm
+if( nargin<3 || isempty(x3) ), x3=0;  end
+if( nargin<4 || isempty(prm) ), prm=[]; end %#ok<NASGU>
 
-% A Blank line right after the function delclaration
-% All indents should be set using "smart indent" from Matlab (2 spaces)
-
-if( nargin < 3 ); x3=0;  end
-
+% indents should be set with Matlab's "smart indent" (with 2 spaces)
 if( nargout==1 )
   y1 = add(x1,x2) + x3;
 else
@@ -64,9 +62,6 @@ else
   y2 = - x1 - x2 - x3;
 end
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% all the way for sub-functions
 function s=add(x,y)
-
+% optional sub function comment
 s=x+y;
