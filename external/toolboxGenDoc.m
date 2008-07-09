fbaf7476cfec3a91c4ef509c16aa2f4399b00e03
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2008-07-09 18:12:46 +0000

    -removing /private directories from doc
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@9861 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- external/toolboxGenDoc.m
+++ external/toolboxGenDoc.m
@@ -18,7 +18,7 @@
 %
 % See also
 %
-% Piotr's Image&Video Toolbox      Version 2.10
+% Piotr's Image&Video Toolbox      Version NEW
 % Copyright 2008 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Lesser GPL [see external/lgpl.txt]
@@ -51,3 +51,8 @@ while ~isempty(d)
   end
   d(1) = [];
 end
+
+% remove /private directories
+rmdir('doc/classify/private','s')
+rmdir('doc/images/private','s')
+
