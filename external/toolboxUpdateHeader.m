46e325985dc77e4f152e9f3d59b0b5cd59f375fe
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2008-07-09 22:58:50 +0000

    -VERSION 2.11 OF TOOLBOX [FINAL]
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@9866 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- external/toolboxUpdateHeader.m
+++ external/toolboxUpdateHeader.m
@@ -14,13 +14,13 @@ function toolboxUpdateHeader
 %
 % See also
 %
-% Piotr's Image&Video Toolbox      Version 2.10
+% Piotr's Image&Video Toolbox      Version 2.11
 % Copyright 2008 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Lesser GPL [see external/lgpl.txt]
 
 header={
-  '% Piotr''s Image&Video Toolbox      Version 2.10'; ...
+  '% Piotr''s Image&Video Toolbox      Version 2.11'; ...
   '% Copyright 2008 Piotr Dollar.  [pdollar-at-caltech.edu]'; ...
   '% Please email me if you find bugs, or have suggestions or questions!'; ...
   '% Licensed under the Lesser GPL [see external/lgpl.txt]'};
