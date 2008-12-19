14a83c5535cd3bc15f408d40bebff04da9286f14
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2008-12-19 00:53:58 +0000

    -IntegralImage.h commenting/clean complete
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@11118 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- boosting/IntegralImage.h
+++ boosting/IntegralImage.h
@@ -1,3 +1,11 @@
+/**************************************************************************
+* Integral Image for fast computation of Haar features (see Haar.h).
+*
+* Piotr's Image&Video Toolbox      Version NEW
+* Copyright 2008 Piotr Dollar.  [pdollar-at-caltech.edu]
+* Please email me if you find bugs, or have suggestions or questions!
+* Licensed under the Lesser GPL [see external/lgpl.txt]
+**************************************************************************/
 #ifndef _ITEGRAL_IMAGE_H_
 #define _ITEGRAL_IMAGE_H_
 
@@ -43,10 +51,8 @@ public:
 
 inline void				IntegralImage::clear()
 {
-	_II.setDims(0,0);
-	_sqrII.setDims(0,0);
-	_w = _h = 0;
-	_roiLf = _roiTp = _roiRt = _roiBt = 0;
+	_II.clear(); _sqrII.clear();
+	_w = _h = _roiLf = _roiTp = _roiRt = _roiBt = 0;
 	_roiMu = 0.0; _roiSig = 1.0; _roiSigInv = 1.0;
 }
 
@@ -54,14 +60,13 @@ inline void				IntegralImage::setRoi(		int lf, int tp, int rt, int bt )
 {
 	if( _roiLf==lf && _roiTp==tp && _roiRt==rt && _roiBt==bt ) return;
 	_roiLf	= _roiTp = 0;
-
 	double areaInv = 1.0/double((rt-lf+1)*(bt-tp+1));
-	double m1 = rectSum(lf, tp, rt, bt)    * areaInv;
-	double m2 = rectSumSq(lf, tp, rt, bt)  * areaInv;
+	double m1 = rectSum(lf, tp, rt, bt) * areaInv;
+	double m2 = rectSumSq(lf, tp, rt, bt) * areaInv;
 	_roiMu = m1;
 	_roiSig = sqrt(max(m2 - m1 * m1, 0.0)) + .000001;
-	_roiSigInv	= 1.0/_roiSig;
-	_roiLf=lf;  _roiTp=tp;  _roiRt=rt; _roiBt=bt;
+	_roiSigInv = 1.0/_roiSig;
+	_roiLf=lf; _roiTp=tp; _roiRt=rt; _roiBt=bt;
 }
 
 inline void				IntegralImage::getRoi(		int &lf, int &tp, int &rt, int &bt )
@@ -88,14 +93,13 @@ template<class T> void	IntegralImage::prepare( const Matrix<T> &I )
 	double v; int i, j;
 	_w = I.cols() + 1;
 	_h = I.rows() + 1;
-
-	// create integral image and square integral image.
-	// 1) set first row/column to II to be 0
-	// 2) create first real row/column of II
-	// 3) create remainder of II
 	_II.setDims( _h, _w ); _sqrII.setDims( _h, _w );
+
+	// 1) set first row/column to II to be 0
 	for( i = 0; i < _w; i++) _II(0, i) = _sqrII(0, i) = 0;
 	for( j = 0; j < _h; j++) _II(j, 0) = _sqrII(j, 0) = 0;
+
+	// 2) create first real row/column of II
 	v = I(0,0); _II(1,1)=v; _sqrII(1,1)=v*v;
 	for( i = 2; i < _w; i++) {
 		v = I(0, i-1);
@@ -107,6 +111,8 @@ template<class T> void	IntegralImage::prepare( const Matrix<T> &I )
 		_II(j, 1) = _II(j-1, 1) + v;
 		_sqrII(j, 1) = _sqrII(j-1, 1) + v*v;
 	}
+
+	// 3) create remainder of II
 	for( j = 2; j < _h; j++) for( i = 2; i < _w; i++) {
 		v = I(j-1, i-1);
 		_II(j, i) = _II(j-1, i) + _II(j, i-1) - _II(j-1, i-1) + v;
