bb50d88795fe55b2e50679660574e33827132680
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2009-02-23 00:16:13 +0000

    -updated Contents.m w imDownsample.m
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@12109 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/Contents.m
+++ images/Contents.m
@@ -23,6 +23,7 @@
 %   xeucn          - n-dimensional euclidean distance between each window in A and template T.
 %
 % Image deformation:
+%   imDownsample   - Fast bilinear image downsampling.
 %   imNormalize    - Various ways to normalize a (multidimensional) image.
 %   imShrink       - Used to shrink a multidimensional array I by integer amount.
 %   imtransform2   - Applies a general/special homography on an image I
