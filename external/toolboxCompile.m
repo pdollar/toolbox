c7116ebeb4b9f9816ae7b5fa5f5a16ee70bbcd4c
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2009-03-06 23:47:58 +0000

    -updated / fixed (bad mofo) compile script
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@12391 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- external/toolboxCompile.m
+++ external/toolboxCompile.m
@@ -20,32 +20,32 @@
 
 disp('Compiling.......................................');
 
-optionsLinux = {};
-if  strcmp(computer,'GLNX86')
-  optionsLinux  = { 'CXX=g++-4.1' 'CC=g++-4.1' 'LD=g++-4.1' };
+% general compile options
+opts0 = {'-outdir'};
+if(strcmp(computer,'GLNX86'))
+  opts0=['CXX=g++-4.1' 'CC=g++-4.1' 'LD=g++-4.1' opts0];
 end
 
-dir = 'classify/private/';
-options = { '-outdir' dir optionsLinux{:} };
-mex([dir 'meanShift1.c'],           options{:} );
+dir='classify/private/'; opts=[opts0 dir];
+mex([dir 'meanShift1.c'],           opts{:} );
 
-dir = 'images/private/';
-options = { '-outdir' dir optionsLinux{:} };
-mex([dir 'assignToBins1.c'],        options{:} );
-mex([dir 'histc2c.c'],              options{:} );
-mex([dir 'ktHistcRgb_c.c'],         options{:} );
-mex([dir 'ktComputeW_c.c'],         options{:} );
-mex([dir 'maskEllipse1.c'],         options{:} );
-mex([dir 'nlfiltersep_max.c'],      options{:} );
-mex([dir 'nlfiltersep_sum.c'],      options{:} );
-mex([dir 'nlfiltersep_blocksum.c'], options{:} );
-mex([dir 'imDownsample.c'],         options{:} );
+dir='images/private/'; opts=[opts0 dir];
+mex([dir 'assignToBins1.c'],        opts{:} );
+mex([dir 'histc2c.c'],              opts{:} );
+mex([dir 'ktHistcRgb_c.c'],         opts{:} );
+mex([dir 'ktComputeW_c.c'],         opts{:} );
+mex([dir 'maskEllipse1.c'],         opts{:} );
+mex([dir 'nlfiltersep_max.c'],      opts{:} );
+mex([dir 'nlfiltersep_sum.c'],      opts{:} );
+mex([dir 'nlfiltersep_blocksum.c'], opts{:} );
+
+dir='images/private/'; opts=[opts0 'images/'];
+mex([dir 'imDownsample.c'],         opts{:} );
 
 try
   % requires c++ compiler
-  dir='matlab/private/';
-  options={'-output', 'dijkstra', '-outdir', 'matlab', optionsLinux{:} };
-  mex([dir 'fibheap.cpp'],[dir 'dijkstra.cpp'], options{:} );
+  dir='matlab/private/'; opts=[opts0 'matlab' '-output' 'dijkstra'];
+  mex([dir 'fibheap.cpp'],[dir 'dijkstra.cpp'], opts{:} );
 catch ME
   fprintf(['Dijkstra''s shortest path algorithm compile failed,\n' ...
     'most likely due to lack of a C++ compiler.\n' ...
@@ -55,4 +55,4 @@ catch ME
     'dijkstra.cpp fibheap.cpp fibheap.h\n']);
 end
 
-disp('.......................................Done Compiling');
+disp('..................................Done Compiling');
