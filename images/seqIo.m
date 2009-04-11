d02bf7fa88be5a4de0060e2a8ee9ecd3827d56d4
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2009-04-11 01:25:23 +0000

    -seqIo.m minor (no longer needed getExt())
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@12911 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/seqIo.m
+++ images/seqIo.m
@@ -113,7 +113,7 @@ elseif( strcmp(mode,'crop') )
   
 elseif( strcmp(mode,'toimgs') )
   d=varargin{1}; if(~exist(d,'dir')), mkdir(d); end
-  sr=seqIo(fName,'r'); i=0; ext=getExt(sr.getinfo());
+  sr=seqIo(fName,'r'); i=0; info=sr.getinfo(); ext=info.ext;
   while( 1 )
     f=[d '/I' int2str2(i,5) ext]; i=i+1; if(~sr.next()), break; end
     I=sr.getframeb(); f=fopen(f,'w'); assert(f>0); fwrite(f,I); fclose(f);
@@ -122,7 +122,7 @@ elseif( strcmp(mode,'toimgs') )
   
 elseif( strcmp(mode,'frimgs') )
   d=varargin{1}; info=varargin{2}; assert(exist(d,'dir')==7);
-  sw=seqIo(fName,'w',info); i=0; ext=getExt(info);
+  sw=seqIo(fName,'w',info); i=0; info=sw.getinfo(); ext=info.ext;
   while( 1 )
     f=[d '/I' int2str2(i,5) ext]; i=i+1; if(~exist(f,'file')), break; end
     f=fopen(f,'r'); assert(f>0); I=fread(f); fclose(f); sw.addframeb(I);
@@ -132,14 +132,6 @@ elseif( strcmp(mode,'frimgs') )
 else assert(0);
   
 end
-
-  function ext = getExt( info )
-    switch info.imageFormat
-      case {100,200}, ext='.raw';
-      case {102,201}, ext='.jpg';
-      otherwise, assert(false);
-    end
-  end
 
   function sobj = seqReaderDual( fName1, fName2 )
     s1=srp('open',int32(-1),fName1); s2=srp('open',int32(-1),fName2);
