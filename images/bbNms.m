026bc8c45c04069539d2672946bab2802d3cfa23
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2009-07-07 21:37:20 +0000

    -added bbNms.m: Non-maximal suppression of bbs
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@14151 52fe0c90-79fe-0310-8a18-a0b98ad248f8

---
+++ images/bbNms.m
@@ -0,0 +1,115 @@
+function bbs = bbNms( bbs, varargin )
+% Non-maximal suppression (nms) of bbs (mean-shift or maximum).
+%
+% type=='ms': Mean shift non-maximal suppression (nms) of bbs w variable
+% width kernel. radii controls the amount of suppression. radii is a 4
+% element vector containing the radius for each dimension (x,y,w,h).
+% Typically the first two elements should be the same, as should the last
+% two. Distance between w/h are computed in log2 space (ie w and w*2 are 1
+% unit apart), and the radii should be set accordingly. radii should change
+% depending on spatial and scale stride of bbs.
+%
+% type=='max': Non-maximal suppression (nms) of bbs using area of overlap
+% criteria. For each pair of bounding boxes, if their overlap, defined by:
+%  overlap(bb1,bb2) = area(intersect(bb1,bb2))/area(union(bb1,bb2))
+% is greater than overlap, then the bb with the lower score is suppressed.
+% In the Pascal critieria two bbs are considered a match if overlap>=.5;
+%
+% Although efficient, this function is O(n^2). To speed things up for large
+% n, can divide data randomly into two sets, run nms on each, combine and
+% run nms on the result. If maxn is specified, will split the set if
+% n>maxn. Note that this is a heuristic and can change the results of nms.
+%
+% USAGE
+%  bbs = bbNms( bbs, [varargin] )
+%
+% INPUTS
+%  bbs        - original bbs (must be of form [x y w h wt])
+%  varargin   - additional params (struct or name/value pairs)
+%   .type       - ['max'] 'max'=area of overlap criteria, 'ms'=mean-shift
+%   .thr        - [0] threshold below which to discard bbs
+%   .maxn       - [500] if n>maxn split and run recursively (see above)
+%   .radii      - [.15 .15 1 1] supression radii ('ms' only, see above)
+%   .overlap    - [.5] area of overlap for bbs ('max' only, see above)
+%
+% OUTPUTS
+%  bbs      - suppressed bbs
+%
+% EXAMPLE
+%  bbs=[0 0 1 1 1; .1 .1 1 1 1.1; 2 2 1 1 1];
+%  bbs1 = bbNms(bbs, 'thr',.5, 'type','max' )
+%  bbs2 = bbNms(bbs, 'thr',.5, 'type','ms')
+%
+% See also bbApply, nonMaxSuprList
+
+% get parameters
+dfs={'type','max','thr',0,'maxn',500,'radii',[.15 .15 1 1],'overlap',.5};
+[type,thr,maxn,radii,overlap]=getPrmDflt(varargin,dfs,1);
+assert(maxn>=2); assert(numel(overlap)==1);
+
+% discard bbs below threshold and run nms1
+kp=bbs(:,5)>thr; bbs=bbs(kp,:); if(size(bbs,1)<=1), return; end;
+bbs = nms1(bbs,type,thr,maxn,radii,overlap);
+
+  function bbs = nms1( bbs, type, thr, maxn, radii, overlap )
+    % if big split in two, recurse, merge, then run on merged
+    if( size(bbs,1)>maxn )
+      n=size(bbs,1); bbs=bbs(randperm(n),:); n2=floor(n/2);
+      bbs0=nms1(bbs(1:n2,:),type,thr,maxn,radii,overlap);
+      bbs1=nms1(bbs(n2+1:n,:),type,thr,maxn,radii,overlap);
+      bbs=[bbs0; bbs1]; n0=n; n=size(bbs,1);
+      if(n<n0), bbs=nms1(bbs,type,thr,maxn,radii,overlap); return; end
+    end
+    % run actual nms on given bbs
+    switch type
+      case 'max', bbs = nmsMax(bbs,overlap);
+      case 'ms', bbs = nmsMs(bbs,thr,radii);
+      otherwise, error('unknown type: %s',type);
+    end
+  end
+
+  function bbs = nmsMax( bbs, overlap )
+    % for each i suppress all j st j>i and overlap>.5
+    [score,ord]=sort(bbs(:,5),'descend'); bbs=bbs(ord,:);
+    n=size(bbs,1); kp=true(1,n); areas=bbs(:,3).*bbs(:,4);
+    xs=bbs(:,1); xe=bbs(:,1)+bbs(:,3); ys=bbs(:,2); ye=bbs(:,2)+bbs(:,4);
+    for i=1:n
+      for j=i+find( kp(i+1:n) )
+        iw=min(xe(i),xe(j))-max(xs(i),xs(j)); if(iw<=0), continue; end
+        ih=min(ye(i),ye(j))-max(ys(i),ys(j)); if(ih<=0), continue; end
+        o=iw*ih; o=o/(areas(i)+areas(j)-o); if(o>overlap), kp(j)=0; end
+      end
+    end
+    bbs=bbs(kp,:);
+  end
+
+  function bbs = nmsMs( bbs, thr, radii )
+    % position = [x+w/2,y+h/2,log2(w),log2(h)], ws=weights-thr
+    ws=bbs(:,5)-thr; w=bbs(:,3); h=bbs(:,4); n=length(w);
+    ps=[bbs(:,1)+w/2 bbs(:,2)+h/2 log2(w) log2(h)];
+    % find modes starting from each elt, then merge nodes that are same
+    ps1=zeros(n,4); ws1=zeros(n,1); stopThr=1e-2;
+    for i=1:n, [ps1(i,:) ws1(i,:)]=nmsMs1(i); end
+    [ps,ws] = nonMaxSuprList(ps1,ws1,stopThr*100,[],[],2);
+    % convert back to bbs format and sort by weight
+    w=pow2(ps(:,3)); h=pow2(ps(:,4));
+    bbs=[ps(:,1)-w/2 ps(:,2)-h/2 w h ws+thr];
+    [ws,ord]=sort(ws,'descend'); bbs=bbs(ord,:);
+    
+    function [p,w]=nmsMs1(ind)
+      % variable bandwith kernel (analytically defined)
+      p=ps(ind,:); [n,m]=size(ps); onesN=ones(n,1);
+      h = [pow2(ps(:,3)) pow2(ps(:,4)) onesN onesN];
+      h = h .* radii(onesN,:); hInv=1./h;
+      while(1)
+        % compute (weighted) squared Euclidean distance to each neighbor
+        d=(ps-p(onesN,:)).*hInv; d=d.*d; d=sum(d,2);
+        % compute new mode
+        wMask=ws.*exp(-d); wMask=wMask/sum(wMask); p1=wMask'*ps;
+        % stopping criteria
+        diff=sum(abs(p1-p))/m; p=p1; if(diff<stopThr), break; end
+      end
+      w = sum(ws.*wMask);
+    end
+  end
+end
