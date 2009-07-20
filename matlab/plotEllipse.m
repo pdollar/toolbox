f8822d69c340d7a2b036d0ec7c4673bcc1906a3c
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2009-07-20 20:07:43 +0000

    -plotEllipse.m: minor tweaks/fixes
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@14315 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- matlab/plotEllipse.m
+++ matlab/plotEllipse.m
@@ -2,7 +2,7 @@ function h = plotEllipse( cRow, cCol, ra, rb, phi, color, nPnts )
 % Adds an ellipse to the current plot.
 %
 % USAGE
-%  h = plotEllipse( cRow, cCol, ra, rb, phi, color, nPnts )
+%  h = plotEllipse( cRow, cCol, ra, rb, phi, [color], [nPnts] )
 %
 % INPUTS
 %  cRow    - the row location of the center of the ellipse
@@ -11,7 +11,7 @@ function h = plotEllipse( cRow, cCol, ra, rb, phi, color, nPnts )
 %  rb      - semi-minor axis radius length (in pixels) of the ellipse
 %  phi     - rotation angle (radians) of semimajor axis from x-axis
 %  color   - ['b'] color for ellipse
-%  nPnts   - [] number of points used to draw each ellipse
+%  nPnts   - [100] number of points used to draw each ellipse
 %
 % OUTPUT
 %  h       - handle to ellipse
@@ -21,23 +21,21 @@ function h = plotEllipse( cRow, cCol, ra, rb, phi, color, nPnts )
 %
 % See also PLOTGAUSSELLIPSES
 %
-% Piotr's Image&Video Toolbox      Version 2.0
+% Piotr's Image&Video Toolbox      Version NEW
 % Copyright 2008 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Lesser GPL [see external/lgpl.txt]
 
 error(nargchk( 5, 7, nargin ));
-if( nargin<6 || isempty(color) ); color = 'b'; end
-if( nargin<7 || isempty(nPnts) ); nPnts = 100; end
-if( length(ra)~=length(rb)); error('length(ra)~=length(rb)'); end
-if( length(cCol)~=length(cRow)); error('length(cCol)~=length(cRow)'); end
+if(nargin<6 || isempty(color)); color = 'b'; end
+if(nargin<7 || isempty(nPnts)); nPnts = 100; end
 
 % plot ellipse (rotate a scaled circle):
 ts = linspace(-pi,pi,nPnts+1);  cts = cos(ts); sts = sin(ts);
 h = plot( ra*cts*cos(-phi) + rb*sts*sin(-phi) + cCol, ...
-  rb*sts*cos(-phi) - ra*cts*sin(-phi) + cRow, color );
+  rb*sts*cos(-phi) - ra*cts*sin(-phi) + cRow, 'Color', color );
 
 % plot center point
-washeld = ishold; if (~washeld); hold('on'); end
+washeld = ishold; if(~washeld); hold('on'); end
 hc = plot( cCol, cRow, 'k+' ); set( hc, 'Color', color );
-if (~washeld); hold('off'); end
+if(~washeld); hold('off'); end
