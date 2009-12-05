0ae8177abb0c993223ed8463eb977b95d2f513b0
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2009-12-05 03:59:00 +0000

    -localSum.m: extra options to compute local mins/maxes
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@15291 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/localSum.m
+++ images/localSum.m
@@ -1,32 +1,35 @@
-function I = localSum( I, dims, shape )
+function I = localSum( I, dims, shape, op )
 % Fast routine for box filtering.
 %
 % Same effect as calling 'C=convn( I, ones(dims), shape)', except more
 % efficient. Computes local sums by using running sums. To get sum in
 % non-overlapping windows, use shape='block'. Equivalent to doing localSum,
-% and then subsampling (except more efficient).
+% and then subsampling (except more efficient). If operation op is set to
+% 'max' or 'min', computes local maxes or mins instead of sums.
 %
 % USAGE
-%  I = localSum( I, dims, shape )
+%  I = localSum( I, dims, [shape], [op] )
 %
 % INPUTS
 %  I       - matrix to compute sum over
 %  dims    - size of volume to compute sum over, can be scalar
 %  shape   - ['full'] 'valid', 'full', 'same', or 'block'
+%  op      - ['sum'] 'max', or 'min'
 %
 % OUTPUTS
 %  C       - matrix of sums
 %
 % EXAMPLE - 1
-%  A = rand(20); dim=11; shape='valid';
-%  B = localSum(A,dim,shape);
-%  C = conv2(A,ones(dim),shape);
-%  diff=B-C; sum(abs(diff(:)))
+%  A = rand(500); dim=11; shape='same'; r=20;
+%  tic, for i=1:r, B = localSum(A,dim,shape); end; toc
+%  tic, for i=1:r, C = conv2(A,ones(dim),shape); end; toc
+%  diff=B-C; im(diff), sum(abs(diff(:)))
 %
 % EXAMPLE - 2
-%  load trees; I=ind2gray(X,map);
-%  I2 = localSum( I, 5, 'block' );
-%  figure(1); im(I); figure(2); im(I2);
+%  load trees; I=ind2gray(X,map); figure(1); im(I);
+%  I1=localSum(I,3,'block','sum'); figure(2); im(I1); title('sum')
+%  I2=localSum(I,3,'block','max'); figure(3); im(I2); title('max')
+%  I3=localSum(I,3,'block','min'); figure(4); im(I3); title('min')
 %
 % See also IMSHRINK
 %
@@ -35,12 +38,21 @@ function I = localSum( I, dims, shape )
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Lesser GPL [see external/lgpl.txt]
 
-if( nargin<3 || isempty(shape));  shape='full'; end;
+if( nargin<3 || isempty(shape));  shape='full'; end
+if( nargin<4 || isempty(op)); op='sum'; end
 
-if( nargin==3 && strcmp(shape,'block') )
-  funH = @sum;
+if( nargin>=3 && strcmp(shape,'block') )
+  switch op
+    case 'sum', fun = @sum;
+    case 'max', fun = @(x,d) max(x,[],d);
+    case 'min', fun = @(x,d) min(x,[],d);
+  end
 else
-  funH = @nlfiltersep_sum;
-end;
+  switch op
+    case 'sum', fun = @nlfiltersep_sum;
+    case 'max', fun = @nlfiltersep_max;
+    case 'min', error('min not implemented');
+  end
+end
 
-I = nlfiltersep( I, dims, shape, funH );
+I = nlfiltersep( I, dims, shape, fun );
