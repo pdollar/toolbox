1adc239a0dc74e2aa61a5aeea68866224b47bd2b
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2010-02-08 18:28:16 +0000

    -randSample.m: minor (added isArray parameter)
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@16008 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- matlab/randSample.m
+++ matlab/randSample.m
@@ -1,4 +1,4 @@
-function r = randSample( n, k )
+function r = randSample( n, k, isArray )
 % Generate values sampled uniformly without replacement from 1:n.
 %
 % If n is an integer, samples k values from 1:n without replacement. If n
@@ -7,11 +7,12 @@ function r = randSample( n, k )
 % Statistics Toolbox.
 %
 % USAGE
-%  r = randSample( n, k )
+%  r = randSample( n, k, [isArray] )
 %
 % INPUTS
 %  n           - specifies sampling source (if array) or range (if scalar)
 %  k           - the number of samples to draw
+%  isArray     - [0] if true treat n as an array (even if has single elt)
 %
 % OUTPUTS
 %  r           - k uniformly generated random samples
@@ -22,12 +23,13 @@ function r = randSample( n, k )
 %
 % See also RANDPERM
 %
-% Piotr's Image&Video Toolbox      Version 2.31
+% Piotr's Image&Video Toolbox      Version NEW
 % Copyright 2009 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Lesser GPL [see external/lgpl.txt]
 
-if( length(n)==1 )
+if( nargin<3 || isempty(isArray) ), isArray=0; end
+if( length(n)==1 && isArray==0 )
   if(k>n), error('Too many samples requested.'); end
   rp = randperm(n); r = rp(1:k);
 else
