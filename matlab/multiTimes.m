e4083c0f8edb92c5a685919cfeb8664b03dd0eb5
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2010-02-15 02:41:43 +0000

    -multiTimes.m: minor
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@16047 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- matlab/multiTimes.m
+++ matlab/multiTimes.m
@@ -69,11 +69,9 @@ switch type
     C = reshape(sum(bsxfun(@times, permute(A,[2,1,3]), ...
       reshape(B,[1 mb nb ])),2),na,nb);
   case 3.2  % C(:,i) = A(:,i)'*B(:,:,i)
-    C = reshape(sum(bsxfun(@times, reshape(A,ma,1,na), B),1), ...
-      nb,na);
+    C = reshape(sum(bsxfun(@times, reshape(A,ma,1,na), B),1), nb,na);
   case 3.3  % C(:,i) = A(:)*B(:,:,i)
-    C = reshape(sum(bsxfun(@times, reshape(A,1,1,ma), B),1), ...
-      nb,ob);
+    C = reshape(sum(bsxfun(@times, reshape(A,1,1,ma), B),1), nb,ob);
   case 4.1  % C(i) = tr(A(:,:,i)'*B(:,:,i))
     C = reshape(sum(sum(A.*B,1),2),1,o);
   otherwise
