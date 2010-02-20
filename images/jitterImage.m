60809d08562a92c116302da90eb17bcfa22ed7c2
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2010-02-20 01:20:22 +0000

    -jitterImage.m: added support for images w multiple channels (e.g., color)
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@16161 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/jitterImage.m
+++ images/jitterImage.m
@@ -28,20 +28,21 @@ function IJ = jitterImage( I, varargin )
 %  function IJ = jitterImage( I, varargin )
 %
 % INPUTS
-%  I          - BW image (MxN) or images (MxNxK), must have odd dims
+%  I          - image (MxN) or images (MxNxK), must have odd dims
 %  varargin   - additional params (struct or name/value pairs)
 %   .maxn        - [inf] maximum jitters to generate (prior to flip)
 %   .nPhi        - [0] number of rotations
 %   .mPhi        - [0] max value for rotation
 %   .nTrn        - [0] number of translations
 %   .mTrn        - [0] max value for translation
-%   .jsiz        - [] Final size of each image in IJ
 %   .flip        - [0] if true then also adds reflection of each image
+%   .jsiz        - [] Final size of each image in IJ
 %   .scls        - [1 1] nScl x 2 array of vert/horiz scalings
 %   .method      - ['linear'] interpolation method for imtransform2
+%   .hasChn      - [0] if true I is MxNxC or MxNxCxK
 %
 % OUTPUTS
-%  IJ          - MxNxR or MxNxKxR set of images, R=(nTrn^2*nPhi*nScl)
+%  IJ          - MxNxRxK or MxNxCxKxR set of images, R=(nTrn^2*nPhi*nScl)
 %
 % EXAMPLE
 %  load trees; I=imresize(ind2gray(X,map),[41 41]); clear X caption map
@@ -53,6 +54,10 @@ function IJ = jitterImage( I, varargin )
 %  IJ = jitterImage(I,'nPhi',5,'mPhi',10,'nTrn',3,'mTrn',2); montage2(IJ)
 %  % additionally create multiple scaled versions
 %  IJ = jitterImage(I,'scls',[1 1; 2 1; 1 2; 2 2]); montage2(IJ)
+%  % example on color image (5 images of slight rotations)
+%  I=imResample(imread('peppers.png'),100,100);
+%  IJ=jitterImage(I,'nPhi',5,'mPhi',25,'hasChn',1);
+%  montage2(uint8(IJ),{'hasChn',1})
 %
 % See also jitterVideo, imtransform2
 %
@@ -61,22 +66,13 @@ function IJ = jitterImage( I, varargin )
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Lesser GPL [see external/lgpl.txt]
 
-% Eigenanalysis of IJ can be informative:
-%   I = double(I);
-%   IJ = jitterImage( I, 111, 10, 0, 0 );
-%   IJ = jitterImage( I, 11, 10, 11, 3 ); %slow
-%   [ U, mu, variances ] = pca( IJ );
-%   ks = 0:min(11,size(U,2));   % should need about 4
-%   pcaVisualize( U, mu, variances, IJ, [], ks );
-
 % get additional parameters
-nd=ndims(I); siz=size(I);
-dfs={'maxn',inf, 'nPhi',0, 'mPhi',0, 'nTrn',0, 'mTrn',0, ...
-  'jsiz',siz(1:2), 'flip',0, 'scls',[1 1], 'method','linear'};
-[maxn,nPhi,mPhi,nTrn,mTrn,jsiz,flip,scls,method] = ...
+siz=size(I);
+dfs={'maxn',inf, 'nPhi',0, 'mPhi',0, 'nTrn',0, 'mTrn',0, 'flip',0, ...
+  'jsiz',siz(1:2), 'scls',[1 1], 'method','linear', 'hasChn',0};
+[maxn,nPhi,mPhi,nTrn,mTrn,flip,jsiz,scls,method,hasChn] = ...
   getPrmDflt(varargin,dfs,1);
 if(nPhi<=1), mPhi=0; nPhi=1; end; if(nTrn<=1), mTrn=0; nTrn=1; end
-if((nd~=2 && nd~=3) || length(jsiz)~=2), error('I must be 2D or 3D'); end
 
 % build translations and phis
 phis=linspace(-mPhi,mPhi,nPhi)/180*pi;
@@ -90,11 +86,15 @@ pad=(siz1-siz(1:2))/2; pad=max([ceil(pad) 0],0);
 if(any(pad>0)), I=padarray(I,pad,'replicate','both'); end
 
 % now for each image jitter it
-if( nd==2 )
-  IJ = jitterImage1(I,method,maxn,jsiz,phis,dX,dY,scls,flip);
-elseif( nd==3 )
-  IJ = fevalArrays(I,@jitterImage1,method,maxn,jsiz,phis,dX,dY,scls,flip);
-  IJ = reshape(IJ,size(IJ,1),size(IJ,2),[]);
+nScl=size(scls,1); nTrn=length(dX); nPhi=length(phis);
+nOps=min(maxn,nTrn*nPhi*nScl); if(flip), nOps=nOps*2; end
+if(hasChn), nd=3; jsiz=[jsiz siz(3)]; else nd=2; end
+n=size(I,nd+1); IJ=zeros([jsiz nOps n],class(I));
+prm={method,maxn,jsiz,phis,dX,dY,scls,flip};
+if( hasChn )
+  for i=1:n, IJ(:,:,:,:,i) = jitterImage1(I(:,:,:,i),prm{:}); end
+else
+  for i=1:n, IJ(:,:,:,i) = jitterImage1(I(:,:,i),prm{:}); end
 end
 end
 
@@ -111,15 +111,17 @@ for s=1:nScl, S=[scls(s,1) 0; 0 scls(s,2)];
 end
 % apply transformations (special case for all integer translation)
 if(nOps>maxn), HS=HS(:,:,randSample(nOps,maxn)); nOps=maxn; end
-siz=size(I); IJ=zeros([jsiz nOps],class(I)); I1=I; p=(siz-jsiz)/2;
+siz=size(I); nd=ndims(I); nCh=size(I,3); siz2=siz(1:2);
+I1=I; p=(siz-jsiz)/2; IJ=zeros([jsiz nOps],class(I));
 for i=1:nOps, H=HS(:,:,i); d=H(1:2,3)';
   if( all(all(H(1:2,1:2)==eye(2))) && all(mod(d,1)==0) )
-    s=max(1-d,1); e=min(siz-d,siz); s1=2-min(1-d,1); e1=e-s+s1;
-    I1(s1(1):e1(1),s1(2):e1(2)) = I(s(1):e(1),s(2):e(2));
+    s=max(1-d,1); e=min(siz2-d,siz2); s1=2-min(1-d,1); e1=e-s+s1;
+    I1(s1(1):e1(1),s1(2):e1(2),:) = I(s(1):e(1),s(2):e(2),:);
   else
-    I1 = imtransform2(I,H,method,'crop');
+    for j=1:nCh, I1(:,:,j)=imtransform2(I(:,:,j),H,method,'crop'); end
   end
-  IJ(:,:,i) = I1(p(1)+1:end-p(1),p(2)+1:end-p(2));
+  if(nd==2), IJ(:,:,i)=I1(p(1)+1:end-p(1),p(2)+1:end-p(2));
+  else IJ(:,:,:,i)=I1(p(1)+1:end-p(1),p(2)+1:end-p(2),:); end
 end
-if(flip), IJ=cat(3,IJ,IJ(:,end:-1:1,:)); end
+if(flip), IJ=cat(nd+1,IJ,IJ(:,end:-1:1,:,:)); end
 end
