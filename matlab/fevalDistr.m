ebeef95a6f162611af1421405e3550ecd893a225
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2010-03-15 23:31:40 +0000

    -fevalDistr.m: jobs can return outputs (not limited to writing res to disk)
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@16440 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- matlab/fevalDistr.m
+++ matlab/fevalDistr.m
@@ -1,14 +1,13 @@
-function out = fevalDistr( funNm, jobs, varargin )
+function [out,res] = fevalDistr( funNm, jobs, varargin )
 % Run simple jobs locally or in distributed fashion using queue.
 %
-% Runs "feval(funNm,jobs{i}{:})" for each job either locally or across
+% Runs "r=feval(funNm,jobs{i}{:})" for each job either locally or across
 % cluster. Distributed queuing system must be installed separately. If
 % queuing system is not installed, this function can still be called with
-% either the 'local' or 'pafor' options. jobs should be a cell array of
-% length nJob. Each job itself should be a cell array of parameters to pass
-% to funNm. funNm must be a valid function in the path. The function funNm
-% must return a dummy value, although this value will be ignored. Instead,
-% typically funNm should write its results to disk.
+% either the 'local' or 'parfor' options. jobs should be a cell array of
+% length nJob. Each job should be a cell array of parameters to pass to
+% funNm. funNm must be a function in the path and must return a single
+% value (which may be a dummy value if funNm writes its results to disk).
 %
 % If using type='local', jobs are executed using simple for loop. If using
 % type='parfor', the for loop is a parfor loop, make sure to setup matlab
@@ -17,7 +16,7 @@ function out = fevalDistr( funNm, jobs, varargin )
 % defaults to 'local' if cluster code not found.
 %
 % USAGE
-%  out = fevalDistr( funNm, jobs, [varargin] )
+%  [out,res] = fevalDistr( funNm, jobs, [varargin] )
 %
 % INPUTS
 %  funNm      - name of function that will process jobs
@@ -29,12 +28,19 @@ function out = fevalDistr( funNm, jobs, varargin )
 %
 % OUTPUTS
 %  out        - 1 if jobs completed successfully
+%  res        - [1xnJob] cell array containing results of each job
 %
 % EXAMPLE
+%  n=16; jobs=cell(1,n); for i=1:n, jobs{i}={rand(500),ones(25)}; end
+%  tic, [out,J] = fevalDistr('conv2',jobs,'type','local'); toc,
+%  tic, [out,J] = fevalDistr('conv2',jobs,'type','parfor'); toc,
+%  pDistr={'type','distr','pLaunch',{48,401:408}};
+%  tic, [out,J] = fevalDistr('conv2',jobs,pDistr{:}); toc
+%  figure(1); montage2(cell2array(J))
 %
 % See also controller, queue
 %
-% Piotr's Image&Video Toolbox      Version 2.41
+% Piotr's Image&Video Toolbox      Version NEW
 % Copyright 2009 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Lesser GPL [see external/lgpl.txt]
@@ -45,15 +51,18 @@ if(strcmp(type,'distr') && ~exist('controller.m','file'))
   warning(['distributed queuing not installed,' ...
     ' switching to type=''local''.']); type='local';  %#ok<WNTAG>
 end
-nJob=length(jobs); if(nJob==0), out=1; return; end
+nJob=length(jobs); res=cell(1,nJob); store=(nargout==2);
+if(nJob==0), out=1; return; end
 switch type
   case 'local'
     % run jobs locally using single computational thread
     tid=ticStatus('collecting jobs'); out=1;
-    for i=1:nJob, feval(funNm,jobs{i}{:}); tocStatus(tid,i/nJob); end
+    for i=1:nJob, r=feval(funNm,jobs{i}{:});
+      if(store), res{i}=r; end; tocStatus(tid,i/nJob); end
   case 'parfor'
     % run jobs locally using multiple computational threads
-    parfor i=1:nJob, feval(funNm,jobs{i}{:}); end; out=1;
+    parfor i=1:nJob, r=feval(funNm,jobs{i}{:});
+      if(store), res{i}=r; end; end; out=1;
   case 'distr'
     % run jobs using queuing system
     controller('launchQueue',pLaunch{:});
@@ -68,9 +77,10 @@ switch type
     while( 1 )
       jids1=controller('jobProbe',jids);
       if(isempty(jids1)), pause(.1); continue; end
-      jid=jids1(1); [d,err]=controller('jobRecv',jid);
+      jid=jids1(1); [r,err]=controller('jobRecv',jid);
       if(~isempty(err)), disp('ABORTING'); out=0; break; end
-      k=k+1; tocStatus(tid,k/nJob); if(k==nJob), out=1; break; end
+      k=k+1; if(store), res{k}=r; end
+      tocStatus(tid,k/nJob); if(k==nJob), out=1; break; end
     end; controller('closeQueue');
   otherwise, assert(false);
 end
