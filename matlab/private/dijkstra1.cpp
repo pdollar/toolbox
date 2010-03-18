78a89f856f8cd849b456dfb971e273d672714744
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2010-03-18 01:53:25 +0000

    -standardized c/c++ headers: meanShift1.c, imResample1.c, dijkstra.cpp
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@16522 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- matlab/private/dijkstra1.cpp
+++ matlab/private/dijkstra1.cpp
@@ -1,15 +1,14 @@
 /***************************************************************************
-* DIJKSTRA.CPP
-*
-* Based on ISOMAP code which can be found at http:*isomap.stanford.edu/.
-* See accompanying m file (dijkstra.m) for usage.
-*
-* Piotr's Image&Video Toolbox      Version 2.20
-* Copyright 2009 Piotr Dollar.  [pdollar-at-caltech.edu]
+* Piotr's Image&Video Toolbox      Version 2.50
+* Copyright 2010 Piotr Dollar.  [pdollar-at-caltech.edu]
 * Please email me if you find bugs, or have suggestions or questions!
 * Licensed under the Lesser GPL [see external/lgpl.txt]
 ***************************************************************************/
 
+/***************************************************************************
+* Based on ISOMAP code which can be found at http:*isomap.stanford.edu/.
+* See accompanying m file (dijkstra.m) for usage.
+***************************************************************************/
 #include "mex.h"
 #include "fibheap.h"
 #define DIJKSTRA_CPP
