b88e06f0c0f92aa3926231e7895be02e3436dcf5
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2010-03-18 02:16:15 +0000

    -moving classify/clf* to external/deprecated as clf* is obsolete / not very useful
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@16529 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- classify/Contents.m
+++ classify/Contents.m
@@ -11,8 +11,8 @@
 %
 % Calculating distances efficiently:
 %   distMatrixShow     - Useful visualization of a distance matrix of clustered points.
-%   softMin            - Calculates the softMin of a vector.
 %   pdist2             - Calculates the distance between sets of vectors.
+%   softMin            - Calculates the softMin of a vector.
 %
 % Principal components analysis:
 %   pca                - Principal components analysis (alternative to princomp).
@@ -21,24 +21,9 @@
 %   pcaVisualize       - Visualization of quality of approximation of X given principal comp.
 %   visualizeData      - Project high dim. data unto principal components (PCA) for visualization.
 %
-% Classification methods with a common interface:
-%   demoClassify       - A demo used to test and demonstrate the usage of classifiers (clf*)
-%   nfoldxval          - Runs n-fold cross validation on data with a given classifier.
+% Confusion matrix display.
 %   confMatrix         - Generates a confusion matrix according to true and predicted data labels.
 %   confMatrixShow     - Used to display a confusion matrix.
-%   clfDecTree         - Wrapper for treefit that makes decision trees compatible with nfoldxval.
-%   clfDecTreeFwd      - Apply the decision tree to data X.
-%   clfDecTreeTrain    - Train a decision tree classifier.
-%   clfEcoc            - Wrapper for ecoc that makes ecoc compatible with nfoldxval.
-%   clfEcocCode        - Generates optimal ECOC codes when 3<=nclasses<=7.
-%   clfKnn             - Create a k nearest neighbor classifier.
-%   clfKnnDist         - k-nearest neighbor classifier based on a distance matrix D.
-%   clfKnnFwd          - Apply a k-nearest neighbor classifier to X.
-%   clfKnnTrain        - Train a k nearest neighbor classifier (memorization).
-%   clfLda             - Create a Linear Discriminant Analysis (LDA) classifier.
-%   clfLdaFwd          - Apply the Linear Discriminant Analysis (LDA) classifier to data X.
-%   clfLdaTrain        - Train a Linear Discriminant Analysis (LDA) classifier.
-%   clfSvm             - Wrapper for svm that makes svm compatible with nfoldxval.
 %
 % Radial Basis Functions (RBFs)
 %   rbfComputeBasis    - Get locations and sizes of radial basis functions for use in rbf network.
