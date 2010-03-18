5e02785911ba75eea78b9c581f7ff30aad4e65be
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2010-03-18 01:58:48 +0000

    -standardized c/c++ headers: meanShift1.c, imResample1.c, dijkstra.cpp, hog1.cpp
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@16525 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- classify/private/meanShift1.c
+++ classify/private/meanShift1.c
@@ -1,9 +1,9 @@
-/*********************************************************************
+/***************************************************************************
 * Piotr's Image&Video Toolbox      Version 2.50
 * Copyright 2010 Piotr Dollar.  [pdollar-at-caltech.edu]
 * Please email me if you find bugs, or have suggestions or questions!
 * Licensed under the Lesser GPL [see external/lgpl.txt]
-*********************************************************************/
+***************************************************************************/
 #include <math.h>
 #include <string.h>
 #include "mex.h"
--- images/private/hog1.cpp
+++ images/private/hog1.cpp
@@ -1,9 +1,9 @@
 /***************************************************************************
-* Piotr's Image&Video Toolbox      Version 2.41
+* Piotr's Image&Video Toolbox      Version 2.50
 * Copyright 2009 Piotr Dollar.  [pdollar-at-caltech.edu]
 * Please email me if you find bugs, or have suggestions or questions!
 * Licensed under the Lesser GPL [see external/lgpl.txt]
-****************************************************************************/
+***************************************************************************/
 #include <math.h>
 #include "mex.h"
 
