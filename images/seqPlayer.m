253a57b7abe9740c2f3506b1e3f804fe911ed6dc
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2010-04-06 20:44:22 +0000

    -seqPlayer.m: display name of opened seq file in title
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@16678 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/seqPlayer.m
+++ images/seqPlayer.m
@@ -28,7 +28,7 @@ function seqPlayer( fName, dispFunc )
 %
 % See also SEQIO
 %
-% Piotr's Image&Video Toolbox      Version 2.50
+% Piotr's Image&Video Toolbox      Version NEW
 % Copyright 2010 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Lesser GPL [see external/lgpl.txt]
@@ -288,8 +288,9 @@ if(~isempty(fName)), menuApi.vidOpen(fName); end
     
     function updateMenus()
       m=menu; if(isempty(fVid)), en='off'; else en='on'; end
-      set([m.hVidCls m.hVidAud],'Enable',en);
-      dispApi.requestUpdate();
+      set([m.hVidCls m.hVidAud],'Enable',en); nm='Seq Player';
+      if(~isempty(fVid)), [d,nm1]=fileparts(fVid); nm=[nm ' - ' nm1]; end
+      set(hFig,'Name',nm); dispApi.requestUpdate();
     end
     
     function vidClose()
