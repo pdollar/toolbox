7ae0711df1f9ddd66fa1341cab2b9f1d8d6cdcca
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2010-04-08 21:20:41 +0000

    -seqIo.m: revamping internals [currently broken]
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@16699 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/seqIo.m
+++ images/seqIo.m
@@ -87,72 +87,112 @@ function sobj = seqIo( fName, mode, varargin )
 %
 % EXAMPLE
 %
-% See also SEQPLAYER, SEQREADERPLUGIN, SEQWRITERPLUGIN
+% See also seqPlayer, seqReaderPlugin, seqWriterPlugin
 %
-% Piotr's Image&Video Toolbox      Version 2.50
+% Piotr's Image&Video Toolbox      Version NEW
 % Copyright 2010 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Lesser GPL [see external/lgpl.txt]
 
-srp=@seqReaderPlugin;
-swp=@seqWriterPlugin;
-
 mode=lower(mode);
-if( strcmp(mode,'r') )
-  % sr = seqIo(fName,'r')
-  s = srp( 'open', int32(-1), fName );
-  sobj = struct( 'close',@() srp('close',s), ...
-    'getframe',@() srp('getframe',s), ...
-    'getframeb',@() srp('getframeb',s), 'getts',@() srp('getts',s), ...
-    'getinfo',@() srp('getinfo',s), 'getnext',@() srp('getnext',s), ...
-    'next',@() srp('next',s), 'seek',@(f) srp('seek',s,f), ...
-    'step',@(d) srp('step',s,d));
-  
-elseif( strcmp(mode,'w') )
-  % sw = seqIo(fName,'w',info)
-  s = swp( 'open', int32(-1), fName, varargin{1} );
-  sobj = struct( 'close',@() swp('close',s), ...
-    'addframe',@(varargin) swp('addframe',s,varargin{:}), ...
-    'addframeb',@(varargin) swp('addframeb',s,varargin{:}), ...
-    'getinfo',@() swp('getinfo',s) );
-  
-elseif( strcmp(mode,'rdual') )
-  sobj = seqReaderDual(fName{:});
-  
-elseif( strcmp(mode,'getinfo') )
-  % info = seqIo( fName, 'getinfo' )
-  sr=seqIo(fName,'r'); sobj=sr.getinfo(); sr.close();
-  
-elseif( strcmp(mode,'crop') )
-  % seqIo( fName, 'crop', tName, f0, f1 )
-  % seqIo( fName, 'crop', tName, frames )
-  tName=varargin{1}; fs=varargin{2}; ordered=0;
-  if(nargin==5), fs=fs:varargin{3}; ordered=1; end; fs=fs(:)';
-  sr=seqIo(fName,'r'); info=sr.getinfo();
-  sw=seqIo(tName,'w',info); pad=sr.getnext(); pad(:)=0;
-  for f=fs
-    if( ordered )
-      sr.seek(f); [I,ts]=sr.getframeb(); sw.addframeb(I,ts);
-    elseif( f>=0 )
-      sr.seek(f); I=sr.getframeb(); sw.addframeb(I);
-    else
-      sw.addframe(pad);
-    end
+switch mode
+  case 'r', sobj = reader( fName, varargin{:} );
+  case 'rdual', sobj = readerDual( fName, varargin{:} );
+  case 'w', sobj = writer( fName, varargin{:} );
+  case 'getinfo', sobj = getInfo( fName );
+  case 'crop', crop( fName, varargin{:} );
+  case 'toimgs', toImgs( fName, varargin{:} );
+  case 'frimgs', frImgs( fName, varargin{:} );
+  case 'convert', convert( fName, varargin{:} );
+  case 'header', header( fName, varargin{:} );
+  otherwise, error('seqIo unknown mode: ''%s''',mode);
+end
+end
+
+function sobj = reader( fName, varargin )
+% sr = seqIo(fName,'r')
+srp = @seqReaderPlugin;
+s = srp( 'open', int32(-1), fName );
+sobj = struct( 'close',@() srp('close',s), ...
+  'getframe',@() srp('getframe',s), ...
+  'getframeb',@() srp('getframeb',s), 'getts',@() srp('getts',s), ...
+  'getinfo',@() srp('getinfo',s), 'getnext',@() srp('getnext',s), ...
+  'next',@() srp('next',s), 'seek',@(f) srp('seek',s,f), ...
+  'step',@(d) srp('step',s,d));
+end
+
+function sobj = readerDual( fName1, fName2 )
+s1=srp('open',int32(-1),fName1); s2=srp('open',int32(-1),fName2);
+i1=srp('getinfo',s1); i2=srp('getinfo',s2);
+if( i1.width~=i2.width || i1.height~=i2.height )
+  close(); error('Mismatched videos');
+end
+sobj=struct('close',@close, 'getframe',@getframe, ...
+  'getinfo',@getinfo, 'seek',@seek );
+
+  function out=close(), out=srp('close',s1); srp('close',s2); end
+
+  function [I,t]=getframe()
+    [I1,t1]=srp('getframe',s1); [I2,t2]=srp('getframe',s2);
+    I=[I1 I2]; t=(t1+t2)/2;
   end
-  sw.close(); sr.close();
-  
-elseif( strcmp(mode,'toimgs') )
-  % seqIo( fName, 'toimgs', dir, [skip] )
-  d=varargin{1}; if(~exist(d,'dir')), mkdir(d); end
-  if(nargin==4), skip=varargin{2}; else skip=1; end
-  sr=seqIo(fName,'r'); info=sr.getinfo(); ext=['.' info.ext];
-  for frame = skip-1:skip:info.numFrames-1
-    f=[d '/I' int2str2(frame,5) ext]; sr.seek(frame);
-    I=sr.getframeb(); f=fopen(f,'w'); assert(f>0); fwrite(f,I); fclose(f);
+
+  function info=getinfo(), info=i1; info.width=i1.width+i2.width; end
+
+  function out=seek(f)
+    f2 = round( f/(i1.numFrames-1)*(i2.numFrames-1) );
+    out = srp('seek',s1,f) & srp('seek',s2,f2);
   end
-  sr.close();
-  
-elseif( strcmp(mode,'frimgs') && ischar(varargin{1}) )
+end
+
+function sobj = writer( fName, varargin )
+% sw = seqIo(fName,'w',info)
+swp = @seqWriterPlugin;
+s = swp( 'open', int32(-1), fName, varargin{1} );
+sobj = struct( 'close',@() swp('close',s), ...
+  'addframe',@(varargin) swp('addframe',s,varargin{:}), ...
+  'addframeb',@(varargin) swp('addframeb',s,varargin{:}), ...
+  'getinfo',@() swp('getinfo',s) );
+end
+
+function info = getInfo( fName )
+% info = seqIo( fName, 'getinfo' )
+sr=seqIo(fName,'r'); info=sr.getinfo(); sr.close();
+end
+
+function crop( fName, tName, varargin )
+% seqIo( fName, 'crop', tName, f0, f1 )
+% seqIo( fName, 'crop', tName, frames )
+tName=varargin{1}; fs=varargin{2}; ordered=0;
+if(nargin==5), fs=fs:varargin{3}; ordered=1; end; fs=fs(:)';
+sr=seqIo(fName,'r'); info=sr.getinfo();
+sw=seqIo(tName,'w',info); pad=sr.getnext(); pad(:)=0;
+for f=fs
+  if( ordered )
+    sr.seek(f); [I,ts]=sr.getframeb(); sw.addframeb(I,ts);
+  elseif( f>=0 )
+    sr.seek(f); I=sr.getframeb(); sw.addframeb(I);
+  else
+    sw.addframe(pad);
+  end
+end
+sw.close(); sr.close();
+end
+
+function toImgs( fName, varargin )
+% seqIo( fName, 'toimgs', dir, [skip] )
+d=varargin{1}; if(~exist(d,'dir')), mkdir(d); end
+if(nargin==4), skip=varargin{2}; else skip=1; end
+sr=seqIo(fName,'r'); info=sr.getinfo(); ext=['.' info.ext];
+for frame = skip-1:skip:info.numFrames-1
+  f=[d '/I' int2str2(frame,5) ext]; sr.seek(frame);
+  I=sr.getframeb(); f=fopen(f,'w'); assert(f>0); fwrite(f,I); fclose(f);
+end
+sr.close();
+end
+
+function frImgs( fName, varargin )
+if( ischar(varargin{1}) )
   % seqIo( fName, 'frimgs', dir, info, [skip] )
   d=varargin{1}; info=varargin{2}; assert(exist(d,'dir')==7);
   if(nargin==5), skip=varargin{3}; else skip=1; end
@@ -163,8 +203,7 @@ elseif( strcmp(mode,'frimgs') && ischar(varargin{1}) )
   end
   sw.close();
   if(frame==skip-1), warning('No images found.'); end %#ok<WNTAG>
-  
-elseif( strcmp(mode,'frimgs') )
+else
   % seqIo( fName, 'frimgs', IS, info )
   IS=varargin{1}; info=varargin{2};
   nd=ndims(IS); if(nd==2), nd=3; end; assert(nd<=4); nFrm=size(IS,nd);
@@ -172,58 +211,34 @@ elseif( strcmp(mode,'frimgs') )
   if(nd==3), for f=1:nFrm, sw.addframe(IS(:,:,f)); end; end
   if(nd==4), for f=1:nFrm, sw.addframe(IS(:,:,:,f)); end; end
   sw.close();
-  
-elseif( strcmp(mode,'convert') )
-  % seqIo( fName, 'convert', tName, imgFun, [info], [skip] )
-  tName=varargin{1}; imgFun=varargin{2}; assert(~strcmp(tName,fName));
-  if(nargin>=5), info=varargin{3}; else info=[]; end
-  if(nargin>=6), skip=varargin{4}; else skip=1; end
-  sr=seqIo(fName,'r'); if(isempty(info)), info=sr.getinfo(); end
-  I=sr.getnext(); info.width=size(I,2); info.height=size(I,1);
-  sw=seqIo(tName,'w',info);
-  for frame = skip-1:skip:info.numFrames-1
-    sr.seek(frame); [I,ts]=sr.getframe(); I=imgFun(I); sw.addframe(I,ts);
-  end
-  sw.close(); sr.close();
-  
-elseif( strcmp(mode,'header') )
-  % seqIo(fName,'header',info)
-  [d,n]=fileparts(fName); if(isempty(d)), d='.'; end
-  fName=[d '/' n]; oName=[fName '-' datestr(now,30)];
-  if(exist([fName '-seek.mat'],'file')); delete([fName '-seek.mat']); end
-  movefile([fName '.seq'],[oName '.seq'],'f');
-  hr = srp( 'open', int32(-1), oName, varargin{1} );
-  info = seqReaderPlugin('getinfo',hr);
-  hw = swp( 'open', int32(-1), fName, info );
-  for frame = 0:info.numFrames-1, srp('next',hr);
-    [I,ts]=srp('getframeb',hr); swp('addframeb',hw,I,ts); end
-  srp('close',hr); swp('close',hw);
-else
-  error('seqIo unknown mode: ''%s''',mode);
+end
 end
 
-  function sobj = seqReaderDual( fName1, fName2 )
-    s1=srp('open',int32(-1),fName1); s2=srp('open',int32(-1),fName2);
-    i1=srp('getinfo',s1); i2=srp('getinfo',s2);
-    if( i1.width~=i2.width || i1.height~=i2.height )
-      close(); error('Mismatched videos');
-    end
-    sobj=struct('close',@close, 'getframe',@getframe, ...
-      'getinfo',@getinfo, 'seek',@seek );
-    
-    function out=close(), out=srp('close',s1); srp('close',s2); end
-    
-    function [I,t]=getframe()
-      [I1,t1]=srp('getframe',s1); [I2,t2]=srp('getframe',s2);
-      I=[I1 I2]; t=(t1+t2)/2;
-    end
-    
-    function info=getinfo(), info=i1; info.width=i1.width+i2.width; end
-    
-    function out=seek(f)
-      f2 = round( f/(i1.numFrames-1)*(i2.numFrames-1) );
-      out = srp('seek',s1,f) & srp('seek',s2,f2);
-    end
-  end
+function convert( fName, varargin )
+% seqIo( fName, 'convert', tName, imgFun, [info], [skip] )
+tName=varargin{1}; imgFun=varargin{2}; assert(~strcmp(tName,fName));
+if(nargin>=5), info=varargin{3}; else info=[]; end
+if(nargin>=6), skip=varargin{4}; else skip=1; end
+sr=seqIo(fName,'r'); if(isempty(info)), info=sr.getinfo(); end
+I=sr.getnext(); info.width=size(I,2); info.height=size(I,1);
+sw=seqIo(tName,'w',info);
+for frame = skip-1:skip:info.numFrames-1
+  sr.seek(frame); [I,ts]=sr.getframe(); I=imgFun(I); sw.addframe(I,ts);
+end
+sw.close(); sr.close();
+end
 
+function header( fName, info )
+% seqIo(fName,'header',info)
+srp=@seqReaderPlugin; swp=@seqWriterPlugin;
+[d,n]=fileparts(fName); if(isempty(d)), d='.'; end
+fName=[d '/' n]; oName=[fName '-' datestr(now,30)];
+if(exist([fName '-seek.mat'],'file')); delete([fName '-seek.mat']); end
+movefile([fName '.seq'],[oName '.seq'],'f');
+hr = srp( 'open', int32(-1), oName, info );
+info = seqReaderPlugin('getinfo',hr);
+hw = swp( 'open', int32(-1), fName, info );
+for frame = 0:info.numFrames-1, srp('next',hr);
+  [I,ts]=srp('getframeb',hr); swp('addframeb',hw,I,ts); end
+srp('close',hr); swp('close',hw);
 end
