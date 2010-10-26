53acae6874a002160fdcb0fae9bb582cc731d02f
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2010-10-26 21:43:54 +0000

    localSum.m: check if can call convOnes for faster computation
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@18123 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/localSum.m
+++ images/localSum.m
@@ -43,6 +43,21 @@ if( nargin<4 || isempty(op)), op='sum'; end
 assert(any(strcmp(shape,{'same','valid','full','block'})));
 assert(any(strcmp(op,{'sum','max','min'})));
 
+% special case where can compute using convOnes1
+if( all(mod(dims,2)) && ndims(I)<=3 ...
+    && ~strcmp(shape,'block') && strcmp(op,'sum') )
+  siz=size(I); if(length(siz)==2), siz=[siz(:)' 1]; end; r=(dims-1)/2;
+  if(length(r)==1), r=[r r 0]; end; if(length(r)==2), r=[r(:); 0]; end
+  if(strcmp(shape,'valid') && any((2*r+1)>siz)); I=[]; return; end
+  if(strcmp(shape,'full')), s=1+r; e=siz+r; T=I;
+    I=zeros(siz+2*r); I(s(1):e(1),s(2):e(2),s(3):e(3))=T; end
+  I = convOnes1(I,r(1),r(2),r(3));
+  if(strcmp(shape,'valid')), s=1+r; e=siz-r;
+    I=I(s(1):e(1),s(2):e(2),s(3):e(3)); end
+  return;
+end
+
+% standard case (use nlfiltersep)
 if( nargin>=3 && strcmp(shape,'block') )
   switch op
     case 'sum', fun = @sum;
@@ -56,5 +71,4 @@ else
     case 'min', error('min not implemented');
   end
 end
-
 I = nlfiltersep( I, dims, shape, fun );
