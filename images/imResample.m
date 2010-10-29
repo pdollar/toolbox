0147a27d24f4cd42e5ab9ac3bd2d591474369349
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2010-10-29 23:16:14 +0000

    imResample.m: added case for fast nearest neighbor image resampling
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@18150 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/imResample.m
+++ images/imResample.m
@@ -6,32 +6,50 @@ function B = imResample( A, varargin )
 % pixel. Inspired by fast downsapmling routine resize.cpp by Deva Ramanan.
 %
 % USAGE
-%  B = imResample( A, scale )
-%  B = imResample( A, h, w )
+%  B = imResample( A, scale, [method] )
+%  B = imResample( A, h, w, [method] )
 %
 % INPUT [1]
 %  A        - input image (2D or 3D double or uint8 array)
 %  scale    - size(B)=size(A)*scale
+%  method   - ['bilinear'] either 'bilinear' or 'nearest'
 %
 % INPUT [2]
 %  A        - input image (2D or 3D double or uint8 array)
 %  h        - height(B)=h
 %  w        - width(B)=w
+%  method   - ['bilinear'] either 'bilinear' or 'nearest'
 %
 % OUPUT
 %   B       - resampled image
 %
 % EXAMPLE
-%  I=double(imread('cameraman.tif')); n=100; s=.5;
-%  tic, for i=1:n, I1=imresize(I,s,'bilinear','Antialiasing',0); end; toc
-%  tic, for i=1:n, I2=imResample(I,s); end; toc
+%  I=double(imread('cameraman.tif')); n=100; s=.5; method='bilinear';
+%  tic, for i=1:n, I1=imresize(I,s,method,'Antialiasing',0); end; toc
+%  tic, for i=1:n, I2=imResample(I,s,method); end; toc
 %  figure(1); im(I1); figure(2); im(I2); figure(3); im(abs(I1-I2));
 %
 % See also IMRESIZE
 %
-% Piotr's Image&Video Toolbox      Version 2.42
+% Piotr's Image&Video Toolbox      Version NEW
 % Copyright 2010 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Lesser GPL [see external/lgpl.txt]
 
-B = imResample1( A, varargin{:} );
+% figure out method
+if(isnumeric(varargin{end})), bilinear=1; else
+  bilinear=~strcmpi(varargin{end},'nearest');
+  varargin=varargin(1:end-1);
+end
+
+% use bilinear interpolation
+if(bilinear), B=imResample1(A,varargin{:}); return; end
+
+% use nearest neighbor interpolation
+m=size(A,1); n=size(A,2); nd=ndims(A);
+if(nargin<=3), sy=varargin{1}; sx=sy; m1=ceil(m*sy); n1=ceil(n*sx);
+else m1=varargin{1}; n1=varargin{2}; sy=m1/m; sx=n1/n; end
+y=(1:m1)'; y=floor(y/sy-.5/sy+1); y=min(max(1,y),m);
+x=(1:n1)'; x=floor(x/sx-.5/sx+1); x=min(max(1,x),n);
+if(nd==2), B=A(y,x); elseif(nd==3), B=A(y,x,:); else
+  ids={y,x}; ids(3:nd)={':'}; B=A(ids{:}); end
