2661483b67ebf88b5e489f7b546371ff8600255a
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2011-02-04 04:47:42 +0000

    imtransform2_apply.*: minor fix
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@19050 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/private/imtransform2_apply.c
+++ images/private/imtransform2_apply.c
@@ -8,11 +8,12 @@
 
 void			mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   /* J=imtransform2_apply(I,rs,cs,is,flag); */
-  int flag, m, n, i, id, fr, fc, *is; double *I, *J, *rs, *cs;
+  int flag, m, n, m1, n1, i, id, fr, fc, *is; double *I, *J, *rs, *cs;
   double wr, wc, wrc, r, c;
 
   /* extract inputs */
-  m = mxGetM(prhs[1]); n = mxGetN(prhs[1]);
+  m = mxGetM(prhs[0]); n = mxGetN(prhs[0]);
+  m1 = mxGetM(prhs[1]); n1 = mxGetN(prhs[1]);
   I   = (double*) mxGetData(prhs[0]);
   rs  = (double*) mxGetData(prhs[1]);
   cs  = (double*) mxGetData(prhs[2]);
@@ -20,11 +21,11 @@ void			mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   flag = (int) mxGetScalar(prhs[4]);
 
   /* Perform interpolation */
-  J = mxMalloc(sizeof(double)*m*n);
+  J = mxMalloc(sizeof(double)*m1*n1);
   if( flag==1 ) { /* nearest neighbor */
-    for( i=0; i<m*n; i++ ) J[i]=I[is[i]];
+    for( i=0; i<m1*n1; i++ ) J[i]=I[is[i]];
   } else { /* bilinear */
-    for( i=0; i<m*n; i++ ) {
+    for( i=0; i<m1*n1; i++ ) {
       id=is[i]; wr=rs[i]; wc=cs[i]; wrc=wr*wc;
       J[i]=I[id]*(1-wr-wc+wrc) + I[id+1]*(wr-wrc) + I[id+m]*(wc-wrc) + I[id+m+1]*wrc;
     }
@@ -32,5 +33,5 @@ void			mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 
   /* create output array */
   plhs[0] = mxCreateNumericMatrix(0,0,mxDOUBLE_CLASS,mxREAL);
-  mxSetData(plhs[0],J); mxSetM(plhs[0],m); mxSetN(plhs[0],n);
+  mxSetData(plhs[0],J); mxSetM(plhs[0],m1); mxSetN(plhs[0],n1);
 }
