1de2815f9f7589f1564418dd4d34d0131d5ef9a6
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2011-02-07 20:37:39 +0000

    imtransform2_apply.*: added support for color images
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@19061 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/private/imtransform2_apply.c
+++ images/private/imtransform2_apply.c
@@ -8,30 +8,41 @@
 
 void			mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   /* J=imtransform2_apply(I,rs,cs,is,flag); */
-  int flag, m, n, m1, n1, i, id, *is;
-  double *I, *J, *rs, *cs, wr, wc, wrc;
+  int flag, *nsI, nsJ[3], areaJ, areaI, nDims, i, k, id, *is;
+  double *I, *J, *I1, *J1, *rs, *cs, wr, wc, wrc;
 
   /* extract inputs */
-  m = mxGetM(prhs[0]); n = mxGetN(prhs[0]);
-  m1 = mxGetM(prhs[1]); n1 = mxGetN(prhs[1]);
   I   = (double*) mxGetData(prhs[0]);
   rs  = (double*) mxGetData(prhs[1]);
   cs  = (double*) mxGetData(prhs[2]);
   is  = (int*) mxGetData(prhs[3]);
   flag = (int) mxGetScalar(prhs[4]);
 
+  /* get dimensions */
+  nDims = mxGetNumberOfDimensions(prhs[0]);
+  nsI = (int*) mxGetDimensions(prhs[0]);
+  nsJ[0]=mxGetM(prhs[1]); nsJ[1]=mxGetN(prhs[1]);
+  nsJ[2]=(nDims==2) ? 1 : nsI[2];
+  areaJ=nsJ[0]*nsJ[1]; areaI=nsI[0]*nsI[1];
+
   /* Perform interpolation */
-  J = mxMalloc(sizeof(double)*m1*n1);
+  J = mxMalloc(sizeof(double)*areaJ*nsJ[2]);
   if( flag==1 ) { /* nearest neighbor */
-    for( i=0; i<m1*n1; i++ ) J[i]=I[is[i]];
+    for( k=0; k<nsJ[2]; k++ ) {
+      J1=J+areaJ*k; I1=I+areaI*k;
+      for( i=0; i<areaJ; i++ ) J1[i]=I1[is[i]];
+    }
   } else { /* bilinear */
-    for( i=0; i<m1*n1; i++ ) {
-      id=is[i]; wr=rs[i]; wc=cs[i]; wrc=wr*wc;
-      J[i]=I[id]*(1-wr-wc+wrc) + I[id+1]*(wr-wrc) + I[id+m]*(wc-wrc) + I[id+m+1]*wrc;
+    for( k=0; k<nsJ[2]; k++ ) {
+      J1=J+areaJ*k; I1=I+areaI*k;
+      for( i=0; i<areaJ; i++ ) {
+        id=is[i]; wr=rs[i]; wc=cs[i]; wrc=wr*wc;
+        J1[i]=I1[id]*(1-wr-wc+wrc) + I1[id+1]*(wr-wrc) + I1[id+nsI[0]]*(wc-wrc) + I1[id+nsI[0]+1]*wrc;
+      }
     }
   }
 
   /* create output array */
   plhs[0] = mxCreateNumericMatrix(0,0,mxDOUBLE_CLASS,mxREAL);
-  mxSetData(plhs[0],J); mxSetM(plhs[0],m1); mxSetN(plhs[0],n1);
+  mxSetData(plhs[0],J); mxSetDimensions(plhs[0],nsJ,3);
 }
