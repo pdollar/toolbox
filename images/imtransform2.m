06d0426fc76dea1d1597c23a6f4fc24f2728b94e
pdollar <pdollar@52fe0c90-79fe-0310-8a18-a0b98ad248f8>
2011-06-16 20:57:24 +0000

    imtransform2.m: updated with new version from imagesAlign/ (major changes include new IO format, updated comments, updated examples)
    git-svn-id: svn+ssh://lumo.ucsd.edu/projects/p1/svnroot/pdollar/toolbox@20475 52fe0c90-79fe-0310-8a18-a0b98ad248f8

--- images/imtransform2.m
+++ images/imtransform2.m
@@ -1,5 +1,5 @@
-function J = imtransform2( I, varargin )
-% Applies a general/special homography on an image I
+function J = imtransform2( I, H, varargin )
+% Applies a linear or nonlinear transformation to an image I.
 %
 % Takes the center of the image as the origin, not the top left corner.
 % Also, the coordinate system is row/column format, so H must be also.
@@ -12,53 +12,50 @@ function J = imtransform2( I, varargin )
 % currently inexact (because of some padding/cropping). Preserves I's type.
 %
 % USAGE
-%  J=imtransform2(I,H,[method],[bbox],[show],[pad],[cache])     % homog.
-%  J=imtransform2(I,angle,[method],[bbox],[show],[pad],[cache]) % rotate
-%  J=imtransform2(I,dx,dy,[method],[bbox],[show],[pad],[cache]) % translate
+%  J = imtransform2( I, H, varargin )
 %
 % INPUTS - common
-%  I       - input image [converted to double]
-%  method  - ['linear'] 'nearest', 'spline', 'cubic' (for interp2)
-%  bbox    - ['loose'] or 'crop'
-%  show    - [0] figure to use for optional display
-%  pad     - [0] padding value (scalar, 'replicate' or 'none')
-%  cache   - [0] optionally cache precomp. for given transform/dims.
-%
-% INPUTS - specific to general homography
-%  H       - 3x3 nonsingular homography matrix
-%
-% INPUTS - specific to rotation
-%  angle   - angle to rotate in degrees
-%
-% INPUTS - specific to translation
-%  dx      - x translation (right)
-%  dy      - y translation (up)
+%  I          - input image [converted to double]
+%  H          - 3x3 nonsingular homography matrix
+%  varargin   - additional params (struct or name/value pairs)
+%    .method    - ['linear'] 'nearest', 'spline', 'cubic' (for interp2)
+%    .bbox      - ['crop'] or 'loose'
+%    .show      - [0] figure to use for optional display
+%    .pad       - [0] padding value (scalar, 'replicate' or 'none')
+%    .useCache  - [0] optionally cache precomp. for given transform/dims.
+%    .us        - [] can specify source r/c for each target (instead of H)
+%    .vs        - [] can specify source r/c for each target (instead of H)
 %
 % OUTPUTS
 %  J       - transformed image
 %
-% EXAMPLE - general homography (rotation + translation)
-%  I=imread('peppers.png'); method='linear';
+% EXAMPLE - rigid transformation (rotation + translation)
+%  I=imread('peppers.png');
 %  R = rotationMatrix(pi/4); T=[1; 3]; H=[R T; 0 0 1];
-%  J = imtransform2(I,H,method,'crop',1,'replicate');
+%  J = imtransform2(I,H,'show',1,'pad','replicate');
 %
 % EXAMPLE - general homography (out of plane rotation)
-%  load trees; I=X; method='nearest';
+%  load trees; I=X;
 %  S=eye(3); S([1 5])=1/500; % zoom out 500 pixels
 %  H=S^-1*rotationMatrix([0 1 0],pi/4)*S;
-%  J = imtransform2(I,H,method,'loose',1);
+%  J = imtransform2(I,H,'bbox','loose','show',1);
 %
-% EXAMPLE - rotation
-%  load trees; I=X; method='bilinear';
-%  tic; J1 = imrotate(I,55,method,'crop'); toc
-%  tic; J2 = imtransform2(I,55,method,'crop'); toc
-%  clf; subplot(2,2,1); im(I); subplot(2,2,2); im(J1-J2);
-%  subplot(2,2,3); im(J1); subplot(2,2,4); im(J2);
-%
-% EXAMPLE - translation
-%  load trees; I=X; method='bicubic';
-%  J = imtransform2(X,0,1.5,method,'crop');
-%  figure(1); clf; im(I,[0 128]); figure(2); clf; im(J,[0 128]);
+% EXAMPLE - rotation using three approaches (and timing)
+%  load trees; I=imResample(X,4); angle=35; method='bilinear';
+%  % (1) rotate using imrotate (slow)
+%  tic; J1 = imrotate(I,angle,method,'crop'); toc
+%  % (2) rotate using a homography matrix
+%  R=rotationMatrix(angle/180*pi); H=[R [0; 0]; 0 0 1];
+%  tic; J2 = imtransform2(I,H,'bbox','crop','method',method); toc
+%  % (3) rotate by explicitly specifying target rs/cs
+%  m=size(I,1)+4; n=size(I,2)+4; m2=(m-1)/2; n2=(n-1)/2;
+%  [cs,rs]=meshgrid(-n2:n2,-m2:m2); vs=R*[cs(:) rs(:)]';
+%  us=reshape(vs(2,:),m,n)-rs; vs=reshape(vs(1,:),m,n)-cs;
+%  tic, J3=imtransform2(I,[],'us',us,'vs',vs,'method',method); toc
+%  % compare all results
+%  figure(1); clf; subplot(3,2,1); im(I); subplot(3,2,2); im(J1);
+%  subplot(3,2,3); im(J2); subplot(3,2,4); im(abs(J1-J2)); title('J1-J2')
+%  subplot(3,2,5); im(J3); subplot(3,2,6); im(abs(J2-J3)); title('J2-J3')
 %
 % See also TEXTUREMAP, INTERP2
 %
@@ -67,37 +64,16 @@ function J = imtransform2( I, varargin )
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Lesser GPL [see external/lgpl.txt]
 
-% parse inputs and switch between cases
-if( nargin>1 && isscalar(varargin{1}) && ...
-    (nargin==2 || ischar(varargin{2})) ) % rotation
-  angle = varargin{1};  angle = angle /180 * pi;
-  H = [rotationMatrix(angle) [0;0]; 0 0 1];
-  J = imtransform2main( I, H, varargin{2:end} );
-  
-elseif( nargin>2 && isscalar(varargin{1}) ...
-    && isscalar(varargin{2}) ) % translation
-  dx=varargin{1}; dy=varargin{2};
-  H = [eye(2) [dy; dx]; 0 0 1];
-  J = imtransform2main( I, H, varargin{3:end} );
-  
-else % presumably a general homography
-  J = imtransform2main( I, varargin{:} );
-end
-end
-
-function J = imtransform2main( I, H, method, bbox, show, pad, useCache )
-
-% check inputs
-if( nargin<3 || isempty(method)), method='linear'; end
-if( nargin<4 || isempty(bbox)), bbox='loose'; end
-if( nargin<5 || isempty(show)), show=0; end
-if( nargin<6 || isempty(pad)), pad=0; end
-if( nargin<7 || isempty(useCache)), useCache=0; end
+% check and parse inputs
+dfs={'method','linear','bbox','crop','show',0,'pad',0,'useCache',0,...
+  'us',[],'vs',[]};
+[method,bbox,show,pad,useCache,us,vs] = getPrmDflt(varargin,dfs,1);
+looseFlag = strcmp(bbox,'loose'); if(show), Iorig=I; end; useH=1;
+if(~isempty(us) && ~isempty(vs)), useH=0; assert(numel(us)==numel(vs)); end
+if( useH && any(size(H)~=[3 3])), error('H must be 3x3'); end
+if( useH && all(all(H==eye(3))) ), J=I; return; end
 if( ndims(I)~=2 && ndims(I)~=3 ), error('I must a MxNXK array'); end
-if( any(size(H)~=[3 3])), error('H must be 3x3'); end
-if( ~any(strcmp(bbox,{'loose','crop'})));
-  error(['illegal value for bbox: ' bbox]); end
-looseFlag = strcmp(bbox,'loose'); if(show), Iorig=I; end
+if(~any(strcmp(bbox,{'loose','crop'}))), error(['invalid bbox: ' bbox]);end
 if(strncmpi(method,'lin',3) || strncmpi(method,'bil',3)), mflag=2;
 elseif(strcmp(method,'nearest') ), mflag=1; else mflag=0; end
 
@@ -110,13 +86,16 @@ end; m=size(I,1); n=size(I,2);
 
 % optionally cache precomputed transformations
 persistent cVals cKeys cCnt; if(isempty(cCnt)), cCnt=0; end; cached=0;
-if(useCache), cKey=[m n H(:)' mflag looseFlag];
+if(useH && useCache), cKey=[m n H(:)' mflag looseFlag];
   if(cCnt>0), id=find(all(cKey(ones(1,cCnt),:)==cKeys(1:cCnt,:),2));
     if(~isempty(id)), [rs,cs,is]=deal(cVals{id}{:}); cached=1; end; end
 end
 
 % perform transform precomputations
-if( ~useCache || ~cached )
+if( ~useH )
+  % compute inds from row/col flow
+  [rs,cs,is]=imtransform2_c('flowToInds',us,vs,m,n,mflag);
+elseif( useH && (~useCache || ~cached) )
   % set origin to be center of image
   r0 = (-m+1)/2; r1 = (m-1)/2;
   c0 = (-n+1)/2; c1 = (n-1)/2;
