5ce65853e5f5503ca6ca82949989c250ffd13f5c
pdollar <pdollar@gmail.com>
2011-08-07 16:42:09 -0700

    hog1.cpp: cleanup

--- images/private/hog1.cpp
+++ images/private/hog1.cpp
@@ -55,20 +55,22 @@ void gradMag(double *I, double *M, int *O, int h, int w, int d, int oBin ) {
 }
 
 /* compute oBin gradient histograms per sBin x sBin block of pixels */
-double *gradHist(double *I, int h, int w, int d, int sBin, int oBin, int oGr) {
+void gradHist( double *M, int *O, double *H, int h, int w, int d,
+  int sBin, int oBin, int oGr, bool soft )
+{
   const int hb=h/sBin, wb=w/sBin, h0=hb*sBin, w0=wb*sBin, nb=wb*hb;
   const double s=sBin, sInv=1/s, sInv2=1/s/s, oGrInv=1/((double)oGr);
-  double *M, *H, *H0; int *O; int x, y, xy, o0, o1, xb0, yb0, oBin1=oBin*nb;
-  double od0, od1, o, m, xb, yb, xd0, xd1, yd0, yd1;
-  /* compute gradient image */
-  M = (double*) mxMalloc(h*w*sizeof(double));
-  O = (int*) mxMalloc(h*w*sizeof(int));
-  gradMag(I, M, O, h, w, d, oBin*oGr);
-  /* compute histograms using M and O */
-  H = (double*) mxCalloc(nb*oBin, sizeof(double));
+  double *H0; int x, y, xy, o0, o1, xb0, yb0, oBin1=oBin*nb;
+  double od0, od1, o, m, m0, m1, xb, yb, xd0, xd1, yd0, yd1;
+  if( !soft || sBin==1 ) { for( x=0; x<w0; x++ ) for( y=0; y<h0; y++ ) {
+    /* interpolate w.r.t. orientation only, not spatial bin */
+    xy=x*h+y; m=M[xy]*sInv2; o=O[xy]*oGrInv; o0=(int) o;
+    m1=(o-o0)*m; m0=m-m1; o0*=nb; o1=o0+nb; if(o1==oBin1) o1=0;
+    H0=H+(x/sBin)*hb+y/sBin; H0[o0]+=m0; H0[o1]+=m1;
+  } return; }
   for( x=0; x<w0; x++ ) for( y=0; y<h0; y++ ) {
     /* get interpolation coefficients */
-    xy=x*h+y; m=M[xy]; o=O[xy]*oGrInv; o0=(int) o;
+    xy=x*h+y; m=M[xy]*sInv2; o=O[xy]*oGrInv; o0=(int) o;
     xb=(((double) x)+.5)*sInv-0.5; xb0=(xb<0) ? -1 : (int) xb;
     yb=(((double) y)+.5)*sInv-0.5; yb0=(yb<0) ? -1 : (int) yb;
     xd0=xb-xb0; xd1=1.0-xd0; yd0=yb-yb0; yd1=1.0-yd0; H0=H+xb0*hb+yb0;
@@ -91,50 +93,66 @@ double *gradHist(double *I, int h, int w, int d, int sBin, int oBin, int oGr) {
       if( xb0+1<wb && yb0+1<hb ) *(H0+hb+1+o1) += od0*xd0*yd0*m;
     }
   }
-  mxFree(M); mxFree(O); return H;
 }
 
-/* compute HOG features */
-mxArray* hog( double *I, int h, int w, int d, int sBin, int oBin, int oGr ) {
-  mxArray *H; double *N, *N1, *hist, *hist1, *H1, n; int o, x, y, x1, y1;
-  const int hb=h/sBin, wb=w/sBin, nb=wb*hb;
-  const int ds[3] = { hb>2?hb-2:0, wb>2?wb-2:0, oBin*4 }, nc=ds[0]*ds[1];
-  /* compute histogram and L2 norm per spatial block */
-  hist = gradHist( I, h, w, d, sBin, oBin, oGr );
-  N = (double*) mxCalloc(nb,sizeof(double));
-  for(o=0; o<oBin; o++) for(x=0; x<nb; x++) N[x]+=hist[x+o*nb]*hist[x+o*nb];
-  /* perform 4 different normalizations per spatial block */
-  H = mxCreateNumericArray(3,ds,mxDOUBLE_CLASS,mxREAL);
-  for( x=0; x<ds[1]; x++ ) for( y=0; y<ds[0]; y++ ) {
-    H1=(double*) mxGetPr(H) + x*ds[0] + y;
+/* compute HOG features given gradient histograms */
+void hog( double *H, double *HG, int h, int w, int d, int sBin, int oBin ) {
+  double *N, *N1, *H1, *HG1, n; int o, x, y, x1, y1, hb, wb, nb, hb1, wb1, nb1;
+  double eps = 1e-4/4.0/sBin/sBin/sBin/sBin; /* precise backward equality */
+  hb=h/sBin; wb=w/sBin; nb=wb*hb; hb1=hb-2; wb1=wb-2; nb1=hb1*wb1;
+  if(hb1<=0 || wb1<=0) return; N = (double*) mxCalloc(nb,sizeof(double));
+  for(o=0; o<oBin; o++) for(x=0; x<nb; x++) N[x]+=H[x+o*nb]*H[x+o*nb];
+  for( x=0; x<wb1; x++ ) for( y=0; y<hb1; y++ ) {
+    HG1 = HG + x*hb1 + y; /* perform 4 normalizations per spatial block */
     for(x1=1; x1>=0; x1--) for(y1=1; y1>=0; y1--) {
-      N1 = N + (x+x1)*hb + (y+y1);  hist1 = hist + (x+1)*hb + (y+1);
-      n = 1.0/sqrt(*N1 + *(N1+1) + *(N1+hb) + *(N1+hb+1) + 0.000025);
-      for(o=0; o<oBin; o++) { *H1=mind(*hist1*n, 0.2); H1+=nc; hist1+=nb; }
+      N1 = N + (x+x1)*hb + (y+y1);  H1 = H + (x+1)*hb + (y+1);
+      n = 1.0/sqrt(*N1 + *(N1+1) + *(N1+hb) + *(N1+hb+1) + eps);
+      for(o=0; o<oBin; o++) { *HG1=mind(*H1*n, 0.2); HG1+=nb1; H1+=nb; }
     }
-  }
-  mxFree(hist); mxFree(N); return H;
+  } mxFree(N);
 }
 
-/* H = hog(I,sBin,oBin,oGr) */
-void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] ) {
-  const int *dims; int sBin, oBin, oGr, nDims, nCh; double *I;
+/* Create [wxhxd] mxArray array, do NOT initialize to 0 */
+mxArray* mxCreateMatrix3(int h, int w, int d, mxClassID id, bool c, void **I ) {
+  const int dims[3]={h,w,d}, n=h*w*d; int b; mxArray* M;
+  if( id==mxINT32_CLASS ) b=sizeof(int);
+  else if( id==mxDOUBLE_CLASS ) b=sizeof(double);
+  else mexErrMsgTxt("Unknown mxClassID.");
+  *I = c ? mxCalloc(n,b) : mxMalloc(n*b);
+  M = mxCreateNumericMatrix(0,0,id,mxREAL);
+  mxSetData(M,*I); mxSetDimensions(M,dims,3); return M;
+}
 
-  /* Error checking */
-  if( nrhs<1 || nrhs>4 ) mexErrMsgTxt("One to four inputs expected.");
-  if( nlhs>1 ) mexErrMsgTxt("One output expected.");
-  nDims=mxGetNumberOfDimensions(prhs[0]);
-  dims = mxGetDimensions(prhs[0]);
-  nCh=(nDims==2) ? 1 : dims[2];
+/* Check inputs and outputs to mex, retrieve first input I */
+void checkArgs( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[],
+  int nl0, int nl1, int nr0, int nr1, int *h, int *w, int *d, double **I )
+{
+  const int *dims; int nDims;
+  if( nlhs<nl0 || nlhs>nl1 ) mexErrMsgTxt("Incorrect number of outputs.");
+  if( nrhs<nr0 || nrhs>nr1 ) mexErrMsgTxt("Incorrect number of inputs.");
+  nDims = mxGetNumberOfDimensions(prhs[0]); dims = mxGetDimensions(prhs[0]);
+  *h=dims[0]; *w=dims[1]; *d=(nDims==2) ? 1 : dims[2];
   if( (nDims!=2 && nDims!=3) || mxGetClassID(prhs[0])!=mxDOUBLE_CLASS)
-    mexErrMsgTxt("I must be a double 2 or 3 dim array.");
+    mexErrMsgTxt("First input must be a double 2 or 3 dimensional array.");
+  *I = (double*) mxGetPr(prhs[0]);
+}
 
-  /* extract input arguments */
-  I = (double*) mxGetPr(prhs[0]);
+/* H = hog( I, [sBin], [oBin], [oGran] ) - see hog.m */
+void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] ) {
+  int h, w, d, hb, wb, nb, hb1, wb1, sBin, oBin, oGr;
+  double *I, *M, *H, *HG; int *O;
+  checkArgs(nlhs,plhs,nrhs,prhs,1,1,1,4,&h,&w,&d,&I);
   sBin = (nrhs>=2) ? (int) mxGetScalar(prhs[1]) : 8;
   oBin = (nrhs>=3) ? (int) mxGetScalar(prhs[2]) : 9;
-  oGr = (nrhs>=4) ? (int) mxGetScalar(prhs[3]) : 10;
-
-  /* compute HOG features */
-  plhs[0] = hog(I, dims[0], dims[1], nCh, sBin, oBin, oGr);
+  oGr  = (nrhs>=4) ? (int) mxGetScalar(prhs[3]) : 10;
+  hb=h/sBin; wb=w/sBin; nb=wb*hb; hb1=hb>2?hb-2:0; wb1=wb>2?wb-2:0;
+  plhs[0] = mxCreateMatrix3( hb1, wb1, oBin*4, mxDOUBLE_CLASS, 0, (void**) &HG);
+  if( hb1==0 || wb1==0 ) return;
+  M = (double*) mxMalloc(h*w*sizeof(double));
+  O = (int*) mxMalloc(h*w*sizeof(int));
+  H = (double*) mxCalloc(nb*oBin, sizeof(double));
+  gradMag( I, M, O, h, w, d, oBin*oGr );
+  gradHist( M, O, H, h, w, d, sBin, oBin, oGr, true );
+  hog( H, HG, h, w, d, sBin, oBin );
+  mxFree(M); mxFree(O); mxFree(H);
 }
