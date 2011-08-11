ae0dea752849387f6b676c191b56e65bbb74f6c2
pdollar <pdollar@gmail.com>
2011-08-10 18:13:30 -0700

    hog1.cpp: factor 2x speedup (copied from channel code)

--- images/private/hog1.cpp
+++ images/private/hog1.cpp
@@ -9,32 +9,24 @@
 #define PI 3.1415926535897931
 double mind(double x, double y) { return (x <= y ? x : y); }
 
-/* snap to one of oBin orientations using binary search */
-int compOrient( double dx, double dy, double *ux, double *uy, int oBin ) {
-  int o0, o1; double s0, s1;
-  if(oBin<=1) return 0; o0=0; o1=oBin-1;
-  s0=fabs(ux[o0]*dx+uy[o0]*dy);
-  s1=fabs(ux[o1]*dx+uy[o1]*dy);
-  while( 1 ) {
-    if(o0==o1-1) { return ((s0>s1) ? o0 : o1); }
-    if( s0<s1 ) {
-      o0+=(o1-o0+1)>>1; s0=fabs(ux[o0]*dx+uy[o0]*dy);
-    } else {
-      o1-=(o1-o0+1)>>1; s1=fabs(ux[o1]*dx+uy[o1]*dy);
-    }
+/* build lookup table a[] s.t. a[(dx+1.1)/2.2*(n-1)]~=acos(dx) */
+float* acosTable() {
+  int i, n=25000; float t, ni;
+  static float a[25000]; static bool init=false;
+  if( init ) return a; ni = 2.2f/(float) n;
+  for( i=0; i<n; i++ ) {
+    t = (i+1)*ni - 1.1f;
+    t = t<-1 ? -1 : (t>1 ? 1 : t);
+    a[i] = (float) acos( t );
   }
+  init=true; return a;
 }
 
 /* compute gradient magnitude and orientation at each location */
-void gradMag(double *I, double *M, int *O, int h, int w, int d, int oBin ) {
-  int x, y, c, o, a=w*h; double m, m1, dx, dx1, dy, dy1, rx, ry;
-  double *ux, *uy, *Ix, *Ix0, *Ix1, *Iy0, *Iy1, *M0; int *O0;
-  /* pre-compute unit vectors evenly distributed at oBin orientations */
-  ux = (double*) mxMalloc(oBin*sizeof(double));
-  uy = (double*) mxMalloc(oBin*sizeof(double));
-  for( o=0; o<oBin; o++ ) ux[o]=cos( ((double)o)/((double)oBin)*PI );
-  for( o=0; o<oBin; o++ ) uy[o]=sin( ((double)o)/((double)oBin)*PI );
-  /* compute gradient magnitude and orientation at each location */
+void gradMag(double *I, double *M, float *O, int h, int w, int d ) {
+  int x, y, c, a=w*h; double m, m1, dx, dx1, dy, dy1, rx, ry;
+  double *Ix, *Ix0, *Ix1, *Iy0, *Iy1, *M0; float o, *O0;
+  float *acost = acosTable(), acMult=(25000-1)/2.2f;
   for( x=0; x<w; x++ ) {
     rx=.5; M0=M+x*h; O0=O+x*h; Ix=I+x*h; Ix0=Ix-h; Ix1=Ix+h;
     if(x==0) { Ix0=Ix; rx=1; } else if(x==w-1) { Ix1=Ix; rx=1; }
@@ -47,35 +39,39 @@ void gradMag(double *I, double *M, int *O, int h, int w, int d, int oBin ) {
         dy1=(*(Iy1+c*a)-*(Iy0+c*a))*ry; dx1=(*(Ix1+c*a)-*(Ix0+c*a))*rx;
         m1=dx1*dx1+dy1*dy1; if(m1>m) { m=m1; dx=dx1; dy=dy1; }
       }
-      *(M0++)=sqrt(m); *(O0++)=compOrient(dx,dy,ux,uy,oBin);
+      if( m==0 ) { o=0; } else {
+        m=sqrt(m); //o=acos(dx/m);
+        o = acost[(int)((dx/m+1.1f)*acMult)];
+        if( o>PI-1e-5 ) o=0; else if( dy<0 ) o=(float)PI-o;
+      }
+      *(M0++) = m; *(O0++) = o;
       Ix0++; Ix1++; Iy0++; Iy1++; Ix++;
     }
   }
-  mxFree(ux); mxFree(uy);
 }
 
 /* compute oBin gradient histograms per sBin x sBin block of pixels */
-void gradHist( double *M, int *O, double *H, int h, int w, int d,
-  int sBin, int oBin, int oGr, bool soft )
+void gradHist( double *M, float *O, double *H, int h, int w, int d,
+  int sBin, int oBin, bool sSoft, bool oSoft )
 {
   const int hb=h/sBin, wb=w/sBin, h0=hb*sBin, w0=wb*sBin, nb=wb*hb;
-  const double s=sBin, sInv=1/s, sInv2=1/s/s, oGrInv=1/((double)oGr);
+  const double s=sBin, sInv=1/s, sInv2=1/s/s, oMult=(double)oBin/PI;
   double *H0; int x, y, xy, o0, o1, xb0, yb0, oBin1=oBin*nb;
   double od0, od1, o, m, m0, m1, xb, yb, xd0, xd1, yd0, yd1;
-  if( !soft || sBin==1 ) { for( x=0; x<w0; x++ ) for( y=0; y<h0; y++ ) {
+  if( !sSoft || sBin==1 ) { for( x=0; x<w0; x++ ) for( y=0; y<h0; y++ ) {
     /* interpolate w.r.t. orientation only, not spatial bin */
-    xy=x*h+y; m=M[xy]*sInv2; o=O[xy]*oGrInv; o0=(int) o;
+    xy=x*h+y; m=M[xy]*sInv2; o=O[xy]*oMult; o0=(int) o;
     m1=(o-o0)*m; m0=m-m1; o0*=nb; o1=o0+nb; if(o1==oBin1) o1=0;
     H0=H+(x/sBin)*hb+y/sBin; H0[o0]+=m0; H0[o1]+=m1;
   } return; }
   for( x=0; x<w0; x++ ) for( y=0; y<h0; y++ ) {
     /* get interpolation coefficients */
-    xy=x*h+y; m=M[xy]*sInv2; o=O[xy]*oGrInv; o0=(int) o;
+    xy=x*h+y; m=M[xy]*sInv2; o=O[xy]*oMult; o0=(int) o;
     xb=(((double) x)+.5)*sInv-0.5; xb0=(xb<0) ? -1 : (int) xb;
     yb=(((double) y)+.5)*sInv-0.5; yb0=(yb<0) ? -1 : (int) yb;
     xd0=xb-xb0; xd1=1.0-xd0; yd0=yb-yb0; yd1=1.0-yd0; H0=H+xb0*hb+yb0;
     /* interpolate using bilinear or trilinear interpolation */
-    if( oGr==1 ) {
+    if( !oSoft || oBin==1 ) {
       o0*=nb;
       if( xb0>=0 && yb0>=0     ) *(H0+o0)      += xd1*yd1*m;
       if( xb0+1<wb && yb0>=0   ) *(H0+hb+o0)   += xd0*yd1*m;
@@ -117,6 +113,7 @@ mxArray* mxCreateMatrix3(int h, int w, int d, mxClassID id, bool c, void **I ) {
   const int dims[3]={h,w,d}, n=h*w*d; int b; mxArray* M;
   if( id==mxINT32_CLASS ) b=sizeof(int);
   else if( id==mxDOUBLE_CLASS ) b=sizeof(double);
+  else if( id==mxSINGLE_CLASS ) b=sizeof(float);
   else mexErrMsgTxt("Unknown mxClassID.");
   *I = c ? mxCalloc(n,b) : mxMalloc(n*b);
   M = mxCreateNumericMatrix(0,0,id,mxREAL);
@@ -137,22 +134,21 @@ void checkArgs( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[],
   *I = (double*) mxGetPr(prhs[0]);
 }
 
-/* H = hog( I, [sBin], [oBin], [oGran] ) - see hog.m */
+/* H = hog( I, [sBin], [oBin] ) - see hog.m */
 void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] ) {
-  int h, w, d, hb, wb, nb, hb1, wb1, sBin, oBin, oGr;
-  double *I, *M, *H, *HG; int *O;
-  checkArgs(nlhs,plhs,nrhs,prhs,1,1,1,4,&h,&w,&d,&I);
+  int h, w, d, hb, wb, nb, hb1, wb1, sBin, oBin;
+  double *I, *M, *H, *HG; float *O;
+  checkArgs(nlhs,plhs,nrhs,prhs,1,1,1,3,&h,&w,&d,&I);
   sBin = (nrhs>=2) ? (int) mxGetScalar(prhs[1]) : 8;
   oBin = (nrhs>=3) ? (int) mxGetScalar(prhs[2]) : 9;
-  oGr  = (nrhs>=4) ? (int) mxGetScalar(prhs[3]) : 10;
   hb=h/sBin; wb=w/sBin; nb=wb*hb; hb1=hb>2?hb-2:0; wb1=wb>2?wb-2:0;
   plhs[0] = mxCreateMatrix3( hb1, wb1, oBin*4, mxDOUBLE_CLASS, 0, (void**) &HG);
   if( hb1==0 || wb1==0 ) return;
   M = (double*) mxMalloc(h*w*sizeof(double));
-  O = (int*) mxMalloc(h*w*sizeof(int));
+  O = (float*) mxMalloc(h*w*sizeof(float));
   H = (double*) mxCalloc(nb*oBin, sizeof(double));
-  gradMag( I, M, O, h, w, d, oBin*oGr );
-  gradHist( M, O, H, h, w, d, sBin, oBin, oGr, true );
+  gradMag( I, M, O, h, w, d );
+  gradHist( M, O, H, h, w, d, sBin, oBin, true, true );
   hog( H, HG, h, w, d, sBin, oBin );
   mxFree(M); mxFree(O); mxFree(H);
 }
