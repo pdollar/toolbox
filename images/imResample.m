d9a74392785fddf1f5c6781a58a5407d5c53bf48
pdollar <pdollar@gmail.com>
2011-12-19 19:45:27 -0800

    images/imResample.m: overhauled mex file (copied from channels/ code but took out SSE for now), changed I/O format

--- images/imResample.m
+++ images/imResample.m
@@ -1,58 +1,56 @@
-function B = imResample( A, varargin )
+function B = imResample( A, scale, method, norm )
 % Fast bilinear image downsampling/upsampling.
 %
 % Gives similar results to imresize with the bilinear option and
 % antialiasing turned off if scale is near 1, except sometimes the final
-% dims are off by 1 pixel. Inspired by fast downsapmling routine resize.cpp
-% by Deva Ramanan. Particularly efficient if downsampling by exact integer
-% value. For very small values of the scale imresize is faster but only
-% looks at subset of values of original image.
+% dims are off by 1 pixel. For very small values of the scale imresize is
+% faster but only looks at subset of values of original image.
 %
 % USAGE
-%  B = imResample( A, scale, [method] )
-%  B = imResample( A, h, w, [method] )
+%  B = imResample( A, scale, [method], [norm] )
 %
-% INPUT [1]
+% INPUT
 %  A        - input image (2D or 3D single, double or uint8 array)
-%  scale    - size(B)=size(A)*scale
+%  scale    - scalar resize factor [s] of target height and width [h w]
 %  method   - ['bilinear'] either 'bilinear' or 'nearest'
-%
-% INPUT [2]
-%  A        - input image (2D or 3D single, double or uint8 array)
-%  h        - height(B)=h
-%  w        - width(B)=w
-%  method   - ['bilinear'] either 'bilinear' or 'nearest'
+%  norm     - [1] optionally multiply every output pixel by norm
 %
 % OUPUT
 %   B       - resampled image
 %
 % EXAMPLE
-%  I=double(imread('cameraman.tif')); n=100; s=1/2; method='bilinear';
+%  I=single(imread('cameraman.tif')); n=100; s=1/2; method='bilinear';
 %  tic, for i=1:n, I1=imresize(I,s,method,'Antialiasing',0); end; toc
 %  tic, for i=1:n, I2=imResample(I,s,method); end; toc
-%  figure(1); im(I1); figure(2); im(I2); figure(3); im(abs(I1-I2));
+%  figure(1); im(I1); figure(2); im(I2);
 %
-% See also IMRESIZE
+% See also imresize
 %
 % Piotr's Image&Video Toolbox      Version NEW
 % Copyright 2011 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Lesser GPL [see external/lgpl.txt]
 
-% figure out method
-if(isnumeric(varargin{end})), bilinear=1; else
-  bilinear=~strcmpi(varargin{end},'nearest');
-  varargin=varargin(1:end-1);
+% figure out method and get target dimensions
+if( nargin<3 || isempty(method) ), bilinear=1; else
+  if(~all(ischar(method))), error('method must be a string'); end
+  bilinear = ~strcmpi(method,'nearest');
 end
+if( nargin<4 || isempty(norm) ), norm=1; end
+[m,n,~]=size(A); k=numel(scale);
 
-% use bilinear interpolation
-if(bilinear), B=imResample1(A,varargin{:}); return; end
-
-% use nearest neighbor interpolation
-m=size(A,1); n=size(A,2); nd=ndims(A);
-if(nargin<=3), sy=varargin{1}; sx=sy; m1=ceil(m*sy); n1=ceil(n*sx);
-else m1=varargin{1}; n1=varargin{2}; sy=m1/m; sx=n1/n; end
-y=(1:m1)'; y=floor(y/sy-.5/sy+1); y=min(max(1,y),m);
-x=(1:n1)'; x=floor(x/sx-.5/sx+1); x=min(max(1,x),n);
-if(nd==2), B=A(y,x); elseif(nd==3), B=A(y,x,:); else
-  ids={y,x}; ids(3:nd)={':'}; B=A(ids{:}); end
+if( bilinear )
+  % use bilinear interpolation
+  if(k==1), m1=round(scale*m); n1=round(scale*n);
+  else m1=scale(1); n1=scale(2); end
+  B=imResampleMex(A,m1,n1,norm);
+else
+  % use nearest neighbor interpolation
+  if(k==1), sy=scale; sx=sy; m1=ceil(m*sy); n1=ceil(n*sx);
+  else m1=scale(1); n1=scale(2); sy=m1/m; sx=n1/n; end
+  y=(1:m1)'; y=floor(y/sy-.5/sy+1); y=min(max(1,y),m);
+  x=(1:n1)'; x=floor(x/sx-.5/sx+1); x=min(max(1,x),n);
+  nd=ndims(A); if(nd==2), B=A(y,x); elseif(nd==3), B=A(y,x,:);
+  else ids={y,x}; ids(3:nd)={':'}; B=A(ids{:}); end
+  if(norm~=1), B=B*norm; end
+end
