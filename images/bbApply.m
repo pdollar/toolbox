f8ae75091f5301fceb628312bb8bb23dbeff4573
pdollar <pdollar@gmail.com>
2012-05-30 11:03:25 -0700

    images/bbApply.m: embed(): minor speedup and fixes

--- images/bbApply.m
+++ images/bbApply.m
@@ -329,24 +329,24 @@ function I = embed( I, bb, varargin )
 % get additional parameters
 dfs={'col',[0 255 0],'lw',3,'fh',35,'fcol',[255 0 0]};
 [col,lw,fh,fcol]=getPrmDflt(varargin,dfs,1);
-n=size(bb,1); bb(:,1:4)=round(bb(:,1:4)); bbI=[1 1 size(I,2) size(I,1)];
-if(size(col,1)==1), col=repmat(col,n,1); end
-if(size(fcol,1)==1), fcol=repmat(fcol,n,1); end
-if( ndims(I)==2 ), I=repmat(I,[1 1 3]); end
+n=size(bb,1); bb(:,1:4)=round(bb(:,1:4));
+if(size(col,1)==1), col=col(ones(1,n),:); end
+if(size(fcol,1)==1), fcol=fcol(ones(1,n),:); end
+if( ndims(I)==2 ), I=I(:,:,[1 1 1]); end
 % embed each bb
-for j=-floor((lw-1)/2):ceil((lw-1)/2)
-  bb1=[bb(:,1:2)-j bb(:,3:4)+2*j]; bb1=intersect(bb1,bbI);
-  x0=bb1(:,1); x1=x0+bb1(:,3)-1; y0=bb1(:,2); y1=y0+bb1(:,4)-1;
-  for b=1:n, if(all(bb1(b,:)==0)), continue; end
-    for c=1:3, I([y0(b) y1(b)],x0(b):x1(b),c)=col(b,c); end
-    for c=1:3, I(y0(b):y1(b),[x0(b) x1(b)],c)=col(b,c); end
-  end
+x0=bb(:,1); x1=x0+bb(:,3)-1; y0=bb(:,2); y1=y0+bb(:,4)-1;
+j0=floor((lw-1)/2); j1=ceil((lw-1)/2); h=size(I,1); w=size(I,2);
+x00=max(1,x0-j0); x01=min(x0+j1,w); x10=max(1,x1-j0); x11=min(x1+j1,w);
+y00=max(1,y0-j0); y01=min(y0+j1,h); y10=max(1,y1-j0); y11=min(y1+j1,w);
+for b=1:n
+  for c=1:3, I([y00(b):y01(b) y10(b):y11(b)],x00(b):x11(b),c)=col(b,c); end
+  for c=1:3, I(y00(b):y11(b),[x00(b):x01(b) x10(b):x11(b)],c)=col(b,c); end
 end
 % embed text displaying bb score (inside upper-left bb corner)
 if(size(bb,2)<5 || fh==0), return; end
-bb(:,1:4)=intersect(bb(:,1:4),bbI);
+bb(:,1:4)=intersect(bb(:,1:4),[1 1 w h]);
 for b=1:n
-  M=char2img(num2str(bb(b,5),4),fh); M=M{1}==0; [h,w]=size(M);
+  M=char2img(sprintf('%.4g',bb(b,5)),fh); M=M{1}==0; [h,w]=size(M);
   y0=bb(b,2); y1=y0+h-1; x0=bb(b,1); x1=x0+w-1;
   if( x0>=1 && y0>=1 && x1<=size(I,2) && y1<=size(I,2))
     Ir=I(y0:y1,x0:x1,1); Ig=I(y0:y1,x0:x1,2); Ib=I(y0:y1,x0:x1,3);
