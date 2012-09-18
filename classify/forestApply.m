5f36b37cb1fccd976cd3063958bda6f90c42bce6
pdollar <pdollar@gmail.com>
2012-09-17 20:23:09 -0700

    *updated headers to reflect toolbox version 3.01*

--- classify/forestApply.m
+++ classify/forestApply.m
@@ -18,7 +18,7 @@ function [hs,probs] = forestApply( data, forest, maxDepth, minCount )
 %
 % See also forestTrain
 %
-% Piotr's Image&Video Toolbox      Version NEW
+% Piotr's Image&Video Toolbox      Version 3.01
 % Copyright 2012 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Simplified BSD License [see external/bsd.txt]
