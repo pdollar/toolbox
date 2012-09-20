fd46e31fcd123015dae60088a390e2ec9167c49a
pdollar <pdollar@gmail.com>
2012-09-19 19:25:26 -0700

    matlab/fevalDistr.m: added case for WINHPC, updated commenting

--- matlab/fevalDistr.m
+++ matlab/fevalDistr.m
@@ -20,13 +20,26 @@ function [out,res] = fevalDistr( funNm, jobs, varargin )
 % queuing system must be installed separately. Currently this option is
 % only supported on the Caltech cluster but could easily be installed on
 % any Linux cluster as it requires only SSH and a shared filesystem.
+% Parameter pLaunch is used for controller('launchQueue',pLaunch{:}) and
+% determines cluster machines used (e.g. pLaunch={48,401:408}).
 %
 % type='COMPILED': jobs are executed locally in parallel by first compiling
-% executables and then running them in background. This option requires the
+% an executable and then running it in background. This option requires the
 % *Matlab Compiler* to be installed (but does NOT require the Parallel
 % Computing Toolbox). Compiling can take 1-10 minutes, so use this option
 % only for large jobs. (On Linux alter startup.m by calling addpath() only
 % if ~isdeployed, otherwise will get error about "CTF" after compiling).
+% Note that relative paths will not work after compiling so all paths used
+% by funNm must be absolute paths.
+%
+% type='WINHPC': jobs are executed on a Windows HPC Server 2008 cluster.
+% Similar to type='COMPILED', except after compiling, the executable is
+% queued to the HPC cluster where all computation occurs. This option
+% likewise requires the *Matlab Compiler*. Paths to data, etc., must be
+% absolute paths and available from HPC cluster. Parameter pLaunch should
+% have two fields 'scheduler' and 'shareDir' that define the HPC Server.
+% For example, at MSR one possible cluster is defined by scheduler =
+% 'MSR-L25-DEV09' and shareDir = '\\MSR-L25-DEV10\share\'.
 %
 % USAGE
 %  [out,res] = fevalDistr( funNm, jobs, [varargin] )
@@ -35,8 +48,8 @@ function [out,res] = fevalDistr( funNm, jobs, varargin )
 %  funNm      - name of function that will process jobs
 %  jobs       - [1xnJob] cell array of parameters for each job
 %  varargin   - additional params (struct or name/value pairs)
-%   .type       - ['local'], 'parfor', 'distr', 'doscompiled'
-%   .pLaunch    - [] parameter to controller('launchQueue',pLaunch{:})
+%   .type       - ['local'], 'parfor', 'distr', 'compiled', 'winhpc'
+%   .pLaunch    - [] extra params for type='distr' or type='winhpc'
 %   .group      - [1] send jobs in batches (only relevant if type='distr')
 %
 % OUTPUTS
@@ -44,12 +57,12 @@ function [out,res] = fevalDistr( funNm, jobs, varargin )
 %  res        - [1xnJob] cell array containing results of each job
 %
 % EXAMPLE
+%  % Note: in this case parallel versions are slower since conv2 is so fast
 %  n=16; jobs=cell(1,n); for i=1:n, jobs{i}={rand(500),ones(25)}; end
-%  tic, [out,J] = fevalDistr('conv2',jobs,'type','local'); toc,
-%  tic, [out,J] = fevalDistr('conv2',jobs,'type','parfor'); toc,
-%  pDistr={'type','distr','pLaunch',{48,401:408}};
-%  tic, [out,J] = fevalDistr('conv2',jobs,pDistr{:}); toc
-%  figure(1); montage2(cell2array(J))
+%  tic, [out,J1] = fevalDistr('conv2',jobs,'type','local'); toc,
+%  tic, [out,J2] = fevalDistr('conv2',jobs,'type','parfor'); toc,
+%  tic, [out,J3] = fevalDistr('conv2',jobs,'type','compiled'); toc
+%  [isequal(J1,J2), isequal(J1,J3)], figure(1); montage2(cell2array(J1))
 %
 % See also matlabpool mcc
 %
@@ -81,7 +94,7 @@ switch lower(type)
   case 'compiled'
     % run jobs locally in background in parallel using compiled code
     t=clock; t=mod(t(end),1); t=round((t+rand)/2*1e15);
-    tDir=sprintf('temp-%15i/',t); mkdir(tDir);
+    tDir=sprintf('temp-%015i/',t); mkdir(tDir);
     fprintf('Compiling (this may take a while)...\n');
     mcc('-m','fevalDistrDisk','-d',tDir,'-a',funNm);
     cmd=[tDir 'fevalDistrDisk ' funNm ' ' tDir ' ']; i=0; k=0;
@@ -95,7 +108,33 @@ switch lower(type)
       for i1=1:k1, [ind,r]=jobLoad(tDir,fs{i1},store); res{ind}=r; end
       tocStatus(tid,k/nJob); if(k==nJob), out=1; break; end
     end
-    for i=1:10,try rmdir(tDir,'s');return;catch,pause(1),end;end%#ok<CTCH>
+    for i=1:10, try rmdir(tDir,'s');break;catch,pause(1),end;end %#ok<CTCH>
+    
+  case 'winhpc'
+    % run jobs using Windows HPC Server
+    dfs={'shareDir','REQ','scheduler','REQ'};
+    [shareDir,scheduler]=getPrmDflt(pLaunch,dfs,1);
+    t=clock; t=mod(t(end),1); t=round((t+rand)/2*1e15);
+    tDir=[shareDir sprintf('\\temp-%015i\\',t)]; mkdir(tDir);
+    fprintf('Compiling (this may take a while)...\n');
+    mcc('-m','fevalDistrDisk','-d',tDir,'-a',funNm);
+    for i=1:nJob, jobSave(tDir,jobs{i},i); end
+    scheduler=[' /scheduler:' scheduler ' '];
+    m=system2(['job new /failontaskfailure:true' scheduler],1);
+    jid=m(isstrprop(m,'digit')); nJobStr=int2str(nJob);
+    system2(['job add ' jid ' -workdir:' tDir ' -parametric:1-' nJobStr ...
+      scheduler ' fevalDistrDisk ' funNm ' ' tDir ' *'],1);
+    system2(['job submit /id:' jid scheduler],1);
+    tid=ticStatus('collecting jobs'); k=0;
+    while( 1 )
+      m=system2(['job view ' jid scheduler],0);
+      [~,j]=regexp(m,'State\s*: '); m1=m(j+1:j+6);
+      if(strcmpi('failed',m1)), disp('ABORTING'); out=0; break; end
+      fs=dir([tDir '*-done']); fs={fs.name}; k1=length(fs); k=k+k1;
+      for i1=1:k1, [ind,r]=jobLoad(tDir,fs{i1},store); res{ind}=r; end
+      tocStatus(tid,k/nJob); if(k==nJob), out=1; break; end
+    end
+    for i=1:10, try rmdir(tDir,'s');break;catch,pause(1),end;end %#ok<CTCH>
     
   case 'distr'
     % run jobs using Linux queuing system
