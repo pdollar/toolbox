a08fe86a39e78a2a5a7ea1c60c3aefeb6e2c082c
pdollar <pdollar@gmail.com>
2013-03-23 17:56:14 -0700

    classify/forestTrain.m: added parameter 'minChild'

--- classify/forestTrain.m
+++ classify/forestTrain.m
@@ -18,6 +18,7 @@ function forest = forestTrain( data, hs, varargin )
 %   .N1       - [5*N/M] number of data points for training each tree
 %   .F1       - [sqrt(F)] number features to sample for each node split
 %   .minCount - [1] minimum number of data points to allow split
+%   .minChild - [1] minimum number of data points allowed at child nodes
 %   .maxDepth - [64] maximum depth of tree
 %   .dWts     - [] weights used for sampling and weighing each data point
 %   .fWts     - [] weights used for sampling features
@@ -34,7 +35,7 @@ function forest = forestTrain( data, hs, varargin )
 % EXAMPLE
 %  N=10000; H=5; d=2; [xs0,hs0,xs1,hs1]=demoGenData(N,N,H,d,1,1);
 %  xs0=single(xs0); xs1=single(xs1);
-%  pTrain={'maxDepth',50,'F1',2,'M',150};
+%  pTrain={'maxDepth',50,'F1',2,'M',150,'minChild',5};
 %  tic, forest=forestTrain(xs0,hs0,pTrain{:}); toc
 %  hsPr0 = forestApply(xs0,forest);
 %  hsPr1 = forestApply(xs1,forest);
@@ -53,10 +54,11 @@ function forest = forestTrain( data, hs, varargin )
 % Licensed under the Simplified BSD License [see external/bsd.txt]
 
 % get additional parameters and fill in remaining parameters
-dfs={'M',1,'N1',[],'F1',[],'minCount',1,'maxDepth',64,'dWts',[],'fWts',[]};
-[M,N1,F1,minCount,maxDepth,dWts,fWts]=getPrmDflt(varargin,dfs,1);
-[N,F]=size(data); assert(length(hs)==N); H=max(hs);
-assert(all(hs>0)); minCount=max(1,minCount);
+dfs={ 'M',1, 'N1',[], 'F1',[], 'minCount',1, 'minChild',1, ...
+  'maxDepth',64, 'dWts',[], 'fWts',[] };
+[M,N1,F1,minCount,minChild,maxDepth,dWts,fWts]=getPrmDflt(varargin,dfs,1);
+[N,F]=size(data); assert(length(hs)==N); assert(all(hs>0)); H=max(hs);
+minChild=max(1,minChild); minCount=max([1 minCount minChild]);
 if(isempty(N1)), N1=round(5*N/M); end; N1=min(N,N1);
 if(isempty(F1)), F1=round(sqrt(F)); end; F1=min(F,F1);
 if(isempty(dWts)), dWts=ones(1,N,'single'); end; dWts=dWts/sum(dWts);
@@ -74,13 +76,13 @@ for i=1:M
     d=wswor(dWts,N1,4); data1=data(d,:); hs1=hs(d);
     dWts1=dWts(d); dWts1=dWts1/sum(dWts1);
   end
-  tree=treeTrain(data1,hs1,H,F1,minCount,maxDepth,dWts1,fWts);
+  tree=treeTrain(data1,hs1,H,F1,minCount,minChild,maxDepth,dWts1,fWts);
   if(i==1), forest=tree(ones(M,1)); else forest(i)=tree; end
 end
 
 end
 
-function tree = treeTrain(data,hs,H,F1,minCount,maxDepth,dWts,fWts)
+function tree=treeTrain(data,hs,H,F1,minCount,minChild,maxDepth,dWts,fWts)
 % Train single random tree.
 N=size(data,1); K=2*N-1;
 thrs=zeros(K,1,'single'); distr=zeros(K,H,'single');
@@ -97,8 +99,8 @@ while( k < K )
   fids1=wswor(fWts,F1,4); data1=data(dids1,fids1);
   [~,order1]=sort(data1); order1=uint32(order1-1);
   [fid,thr,gini]=forestFindThr(data1,hs1,dWts(dids1),order1,H);
-  if( gini<100 )
-    fid=fids1(fid); left=data(dids1,fid)<thr;
+  fid=fids1(fid); left=data(dids1,fid)<thr; count0=nnz(left);
+  if( gini<100 && count0>=minChild && (count(k)-count0)>=minChild )
     child(k)=K; fids(k)=fid-1; thrs(k)=thr;
     dids{K}=dids1(left); dids{K+1}=dids1(~left);
     depth(K:K+1)=depth(k)+1; K=K+2;
