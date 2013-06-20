088921da08742e055343bd5cae291fd9e2b25561
pdollar <pdollar@gmail.com>
2013-06-19 17:41:58 -0700

    classify/demoGenData.m: major cleanup

--- classify/demoGenData.m
+++ classify/demoGenData.m
@@ -1,115 +1,88 @@
-function [X,IDX,T,IDT] = demoGenData(n,m,k,d,c,e,f)
+function [X0,H0,X1,H1] = demoGenData1(n0,n1,k,d,sep,ecc,frc)
 % Generate data drawn form a mixture of Gaussians.
 %
-% Adapted from code by [Nikos Vlassis, 2000].
-% For definitions see [Sanjoy Dasgupta, 1999].
+% For definitions of separation and eccentricity see:
+%  Sanjoy Dasgupta, "Learning Mixtures of Gaussians", FOCS, 1999.
+%  http://cseweb.ucsd.edu/~dasgupta/papers/mog.pdf
 %
 % USAGE
-%  [X,IDX,T,IDT] = demoGenData(n,m,k,d,c,e,[f])
+%  [X0,H0,X1,H1] = demoGenData(n0,n1,k,d,sep,ecc,[frc])
 %
 % INPUTS
-%  n    - size of training set
-%  m    - size of test set
-%  k    - number of components
-%  d    - dimension
-%  c    - separation degree (c>0)
-%  e    - maximum eccentricity (0 < e < 1)
-%  f    - [0] frac of points that are noise (uniformly distributed)
+%  n0     - size of training set
+%  n1     - size of testing set
+%  k      - number of mixture components
+%  d      - data dimension
+%  sep    - separation degree (sep > 0)
+%  ecc    - maximum eccentricity (0 < ecc < 1)
+%  frc    - [0] frac of points that are noise (uniformly distributed)
 %
 % OUTPUTS
-%  X    - training set (n x d)
-%  IDX  - cluster membership [see kmeans2.m]
-%  T    - test set (m x d)
-%  IDT  - cluster membership [see kmeans2.m]
+%  X0     - [n0xd] training set data vectors
+%  H0     - [n0x1] cluster membership in [1,k] (and -1 for noise)
+%  X1     - [n1xd] testing set data vectors
+%  H1     - [n1x1] cluster membership in [1,k] (and -1 for noise)
 %
 % EXAMPLE
-%  [X,IDX,T,IDT] = demoGenData(250,250,4,4,.5,.5,.1);
-%  figure(1); clf; visualizeData( X, 2, IDX ); title('train');
-%  figure(2); clf; visualizeData( T, 2, IDT ); title('test');
+%  n0=1000; k=5; d=2; sep=2; ecc=1; frc=0;
+%  [X0,H0,X1,H1] = demoGenData(n0,n0,k,d,sep,ecc,frc);
+%  figure(1); clf; visualizeData( X0, 2, H0 ); title('train');
+%  figure(2); clf; visualizeData( X1, 2, H1 ); title('test');
 %
-% See also VISUALIZEDATA, DEMOCLUSTER, DEMOCLASSIFY
+% See also visualizeData, demoCluster
 %
-% Piotr's Image&Video Toolbox      Version 2.0
+% Piotr's Image&Video Toolbox      Version NEW
 % Copyright 2012 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Simplified BSD License [see external/bsd.txt]
 
-if( nargin<7 ); f=0; end;
-if (f<0 || f>=1 ); error('illegal value for f'); end
+% generate mixing weights
+W=0; while(any(W<=1/(4*k))), W=rand(k,1); W=W/sum(W); end
 
-% mixing weights
+% adjust n0 and n1 for noise fraction
+if( nargin<7 ), frc=0; end; frc=max(0,min(frc,1));
+if(frc), nn0=floor(frc*n0); nn1=floor(frc*n1); n0=n0-nn0; n1=n1-nn1; end
+
+% create sep-separated Gaussian clusters of maximum eccentricity ecc
+trial = 1;
 while( 1 )
-  W = rand(k,1); W=W/sum(W);
-  if( all(W > 1/(4*k))); break; end
-end
-
-% adjust n and m for noise frac
-if( nargin>=7 && f~=0)
-  nnoise = floor( f * n );
-  mnoise = floor( f * m );
-  n = n - nnoise;  m = m - mnoise;
-end
-
-% create c-separated Gaussian clusters of maximum eccentricity e
-R=zeros(k,d^2);
-trials = 1;
-while( 1 )
-  X = []; IDX = [];
-  T = []; IDT = [];
-  M = randn(k,d)*sqrt(k)*sqrt(c)*trials/10;
-  Trace = zeros(k,1);
+  lam = ones(k,1)/1000;
+  k0=sum(ceil(W*n0)); X0=zeros(k0,d); H0=zeros(k0,1); k0=0;
+  k1=sum(ceil(W*n1)); X1=zeros(k1,d); H1=zeros(k1,1); k1=0;
+  mu = randn(k,d)*sqrt(k)*sqrt(sep)*trial/10;
   for j = 1:k
-    U = rand(d,d)-0.5;
-    U = sqrtm(inv(U*U')) * U;
-    L = diag(rand(d,1)*(e-1)+1).^2/100;
-    msg=1; while( msg ); [C,msg] = chol(U*L*U'); end;
-    R(j,:)=C(:)';
-
-    nj = ceil(n*W(j));
-    Xj = randn(nj,d) * C;
-    IDX = [IDX j(ones(1,nj))]; %#ok<AGROW>
-    X = [X; repmat(M(j,:),nj,1) + Xj]; %#ok<AGROW>
-    Trace(j) = trace(cov(Xj));
-
-    mj = ceil(m*W(j));
-    Tj = randn(mj,d) * C;
-    IDT = [IDT j(ones(1,mj))];     %#ok<AGROW>
-    T = [T; repmat(M(j,:),mj,1) + Tj]; %#ok<AGROW>
+    % generate a random covariance matrix S=C'*C
+    U=rand(d,d)-0.5; U=sqrtm(inv(U*U'))*U;
+    L=diag(rand(d,1)*(ecc-1)+1).^2/100; C=chol(U*L*U');
+    % populate X0, H0
+    nj=ceil(n0*W(j));
+    X0j=randn(nj,d)*C + repmat(mu(j,:),nj,1);
+    H0(k0+1:k0+nj)=j; X0(k0+1:k0+nj,:)=X0j; k0=k0+nj;
+    if(nj>1), lam(j) = sqrt(trace(cov(X0j))); end
+    % populate X1, H1
+    mj=ceil(n1*W(j));
+    X1j=randn(mj,d)*C + repmat(mu(j,:),mj,1);
+    H1(k1+1:k1+mj)=j; X1(k1+1:k1+mj,:)=X1j; k1=k1+mj;
   end
-
-  % check degree of separation (if sufficient, break)
-  er = 0;
-  for i = 1:k-1
-    for j = i+1:k
-      if( norm(M(i,:)-M(j,:)) < c * sqrt(max(Trace(i),Trace(j))) )
-        er = 1;
-      end
-    end
-  end
-  if( ~er ); break; end
-  trials = trials + 1;
+  
+  % check that degree of separation is sufficient (see Dasgupta 99)
+  % use "lam=sqrt(trace(S))" instead of "lam=sqrt(eigs(S,1))*d"
+  S = pdist2(mu,mu,'euclidean'); S(eye(k)>0)=inf;
+  for i=1:k, for j=1:k, S(i,j)=S(i,j)/max(lam(i),lam(j)); end; end
+  if(all(S(:)>=sep)), break; end; trial=trial+1; assert(trial<1000);
 end
 
-% make some uniformly distributed noise
-if( f~=0)
-  if (m>0)
-    maxv=max( max(abs(X(:))), max(abs(T(:))) );
-  else
-    maxv=max(abs(X(:)));
-  end;
-
-  Xnoise = (rand( nnoise, d ) - .5) * maxv * 2.5;
-  IDXnoise = -1;  IDXnoise = IDXnoise( ones(1,nnoise ) );
-  X = [X; Xnoise]; IDX = [IDX IDXnoise];
-  p = randperm( length(IDX) );  X = X(p,:); IDX = IDX(p);
-
-  if( m>0 )
-    Tnoise = (rand( mnoise, d ) - .5) * maxv * 2.5;
-    IDTnoise = -1;  IDTnoise = IDTnoise( ones(1,mnoise ) );
-    T = [T; Tnoise]; IDT = [IDT IDTnoise];
-    p = randperm( length(IDT) );  T = T(p,:); IDT = IDT(p);
-  end
+% generate uniformly distributed noise
+if( frc~=0 )
+  v=max(abs(X0(:))); if(n1), v=max(v,max(abs(X1(:)))); end
+  % populate X0, H0
+  X0j=(rand(nn0,d)-.5)*v*2.5; k0=length(H0);
+  H0(k0+1:k0+nn0)=-1; X0(k0+1:k0+nn0,:)=X0j;
+  k0=k0+nn0; p=randperm(k0); X0=X0(p,:); H0=H0(p);
+  % populate X1, H1
+  X1j=(rand(nn1,d)-.5)*v*2.5; k1=length(H1);
+  H1(k1+1:k1+nn1)=-1; X1(k1+1:k1+nn1,:)=X1j;
+  k1=k1+nn1; p=randperm(k1); X1=X1(p,:); H1=H1(p);
 end
 
-% put into standard form (column format)
-IDX = IDX'; IDT = IDT';
+end
