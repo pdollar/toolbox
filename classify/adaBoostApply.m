4d57f6dc53fe6c1a7e22a37b89dcd82b97151b66
pdollar <pdollar@gmail.com>
2013-07-10 19:31:59 -0700

    classify/adaBoostApply.m: added extra parameters

--- classify/adaBoostApply.m
+++ classify/adaBoostApply.m
@@ -1,12 +1,14 @@
-function hs = adaBoostApply( X, model )
+function hs = adaBoostApply( X, model, maxDepth, minWeight )
 % Apply learned boosted decision tree classifier.
 %
 % USAGE
-%  hs = adaBoostApply( X, model )
+%  hs = adaBoostApply( X, model, [maxDepth], [minWeight] )
 %
 % INPUTS
 %  X          - [NxF] N length F feature vectors (must be type single)
 %  model      - learned boosted tree classifier
+%  maxDepth   - [] maximum depth of tree
+%  minWeight  - [] minimum sample weigth to allow split
 %
 % OUTPUTS
 %  hs         - [Nx1] predicted output log ratios
@@ -21,6 +23,10 @@ function hs = adaBoostApply( X, model )
 % Licensed under the Simplified BSD License [see external/bsd.txt]
 
 assert(isa(X,'single'));
+if(nargin<3 || isempty(maxDepth)), maxDepth=0; end
+if(nargin<4 || isempty(minWeight)), minWeight=0; end
+if(maxDepth>0), model.child(model.depth>=maxDepth) = 0; end
+if(minWeight>0), model.child(model.weights<=minWeight) = 0; end
 nWeak=size(model.fids,2); N=size(X,1); hs=zeros(N,1);
 for i=1:nWeak
   ids = forestInds(X,model.thrs(:,i),model.fids(:,i),model.child(:,i));
