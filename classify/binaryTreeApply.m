1ff3e7288da3aa8366a6d85095f6ae48e63dae3a
pdollar <pdollar@gmail.com>
2013-07-30 19:24:04 -0700

    *updated headers to reflect toolbox version 3.21*

--- classify/binaryTreeApply.m
+++ classify/binaryTreeApply.m
@@ -18,7 +18,7 @@ function hs = binaryTreeApply( X, tree, maxDepth, minWeight, nThreads )
 %
 % See also binaryTreeTrain
 %
-% Piotr's Image&Video Toolbox      Version NEW
+% Piotr's Image&Video Toolbox      Version 3.21
 % Copyright 2013 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Simplified BSD License [see external/bsd.txt]
--- external/toolboxUpdateHeader.m
+++ external/toolboxUpdateHeader.m
@@ -12,13 +12,13 @@ function toolboxUpdateHeader
 %
 % See also
 %
-% Piotr's Image&Video Toolbox      Version 3.20
+% Piotr's Image&Video Toolbox      Version 3.21
 % Copyright 2013 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Simplified BSD License [see external/bsd.txt]
 
 header={
-  '% Piotr''s Image&Video Toolbox      Version 3.20'; ...
+  '% Piotr''s Image&Video Toolbox      Version 3.21'; ...
   '% Copyright 2013 Piotr Dollar.  [pdollar-at-caltech.edu]'; ...
   '% Please email me if you find bugs, or have suggestions or questions!'; ...
   '% Licensed under the Simplified BSD License [see external/bsd.txt]'};
