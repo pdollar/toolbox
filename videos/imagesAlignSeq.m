d64933bf2acd0658de122504682e32bb6b1d7900
pdollar <pdollar@gmail.com>
2013-08-06 13:16:23 -0700

    videos/imagesAlignSeq.m: commenting, working example, input can now be either HxWxN or HxWx3xN

--- videos/imagesAlignSeq.m
+++ videos/imagesAlignSeq.m
@@ -1,47 +1,68 @@
 function [J,Vxs,Vys] = imagesAlignSeq( I, pFlow, type, bndThr )
 % Stabilize image sequence using coarse optical flow estimation.
 %
-% All images in I are warped to the last frame of the sequence. If type>=0
-% coarse optical flow computed via opticalFlow.m with params pFlow is used
-% (with different accumulation options). If type==-1, a homography computed
-% via imagesAlign.m with params pFlow is used.
+% Perform weak image sequence stabilization as described in:
+%  D. Park, C. Zitnick, D. Ramanan and P. Dollár
+%  "Exploring Weak Stabilization for Motion Feature Extraction", CVPR 2013.
+% The approach stabilizes coarse motion due to camera movement but leaves
+% independent object motions intact. This code performs weak sequence
+% stabilization only (no feature extraction), see section 3.1 of the paper.
+% Please cite the above paper if you end up using the stabilization code.
+%
+% Optical flow is computed between all pairs of frames using opticalFlow.m
+% with params 'pFlow'. Flow across multiple frames is accumulated and flow
+% at integer locations at each stage is obtained either using either
+% nearest neighbor (if type==1) or bilinear interpolation (if type==2)
+% which is slightly more accurate but slower. Finally, all images in I are
+% warped to the last frame of the sequence using the accumulated flows. If
+% type==-1, a homography computed via imagesAlign.m with params pFlow is
+% used instead. If videos have black boundaries use bndThr to automatically
+% crop the dark boundaries (with average pixel values under bndThr).
 %
 % USAGE
 %  [J,Vxs,Vys] = imagesAlignSeq( I, pFlow, [type], [bndThr] )
 %
 % INPUTS
-%  I          - [hxwx(3xn)] input rgb sequence of length n
+%  I          - HxWxN or HxWx3xN input image sequence
 %  pFlow      - parameters to use for optical flow computation
-%  type       - [1] accumulation type 0:naive, 1:nearest, 2:bilinear
+%  type       - [1] interpolation type 1:nearest, 2:bilinear
 %  bndThr     - [0] fill in black areas around image boundaries
 %
 % OUTPUTS
-%  J          - [hxwx(3xn)] stabilized image sequence
+%  J          - HxWxN or HxWx3xN stabilized image sequence
+%  Vxs        - HxWxN-1 x-components of flow fields
+%  Vys        - HxWxN-1 y-components of flow fields
 %
 % EXAMPLE
+%  I = seqIo(which('peds30.seq'),'toImgs'); I=I(:,:,:,1:15);
 %  pFlow={'smooth',1,'radius',25,'type','LK','resample',1};
-%  J = imagesAlignSeq( I, pFlow ); % I must be a valid sequence
-%  IJ=[I J]; playMovie(IJ(:,:,1:3:end),15,-1000)
+%  tic, J = imagesAlignSeq( I, pFlow, 1, 20 ); toc
+%  playMovie([I J],15,-10,struct('hasChn',1))
 %
-% See also opticalFlow, imagesAlign
+% See also opticalFlow, imtransform2, imagesAlign
+%
+% Piotr's Image&Video Toolbox      Version NEW
+% Copyright 2013 Piotr Dollar.  [pdollar-at-caltech.edu]
+% Please email me if you find bugs, or have suggestions or questions!
+% Licensed under the Simplified BSD License [see external/bsd.txt]
 
 % default parameters
 if(nargin<3 || isempty(type)), type=1; end
 if(nargin<4 || isempty(bndThr)), bndThr=0; end
 
 % fill in black areas around image boundaries
-[h,w,n]=size(I);
-for t=1:n, if(bndThr<=0), break; end; It=I(:,:,t);
+[h,w,k,n]=size(I); if(n>1), assert(k==3); else n=k; k=1; end
+for t=1:k*n, if(bndThr<=0), break; end; It=I(:,:,t);
   xs=find(sum(It,1)>h*bndThr); ys=find(sum(It,2)>w*bndThr);
   pad=[ys(1)-1 h-ys(end) xs(1)-1 w-xs(end)];
   I(:,:,t)=imPad(imPad(It,-pad,0),pad,'replicate');
 end
-G=rgbConvert(I,'gray'); [h,w,n]=size(G); J=I;
+if(k==1), G=I; else G=rgbConvert(I,'gray'); end; J=I;
 
 % special case if type==-1 use homographies for alignment
 if( type==-1 )
   Hs=zeros(3,3,n-1); Gref=G(:,:,end);
-  for t=1:n-1, is=(1:3)+(t*3-3);
+  for t=1:n-1, is=(1:k)+(t*k-k);
     Hs(:,:,t) = imagesAlign(G(:,:,t),Gref,pFlow);
     J(:,:,is) = imtransform2(I(:,:,is), Hs(:,:,t),'pad','replicate');
   end; Vxs=Hs; Vys=Hs; return;
@@ -54,27 +75,23 @@ for t=1:n-1, [Vxs{t},Vys{t}]=opticalFlow(G(:,:,t+1),G(:,:,t),pFlow); end
 % accumulate flow across frames
 Vx=Vxs{end}; Vy=Vys{end}; [X,Y]=meshgrid(1:w,1:h);
 for t=n-2:-1:1
-  [Vxt,Vyt] = accumulate(Vxs{t},Vys{t},X+Vx,Y+Vy,type);
+  [Vxt,Vyt] = interpolate(Vxs{t},Vys{t},X+Vx,Y+Vy,type);
   Vx=Vx+Vxt; Vxs{t}=Vx; Vy=Vy+Vyt; Vys{t}=Vy;
 end
 
 % transform each I by given flow
-for t=1:n-1, is=(1:3)+(t*3-3); J(:,:,is) = imtransform2(I(:,:,is), ...
+for t=1:n-1, is=(1:k)+(t*k-k); J(:,:,is) = imtransform2(I(:,:,is), ...
     [],'vs',Vxs{t},'us',Vys{t},'pad','replicate'); end
-Vxs=cell2array(Vxs); Vys=cell2array(Vys);
+if(nargout>1), Vxs=cell2array(Vxs); Vys=cell2array(Vys); end
 
 end
 
-function [Vx,Vy] = accumulate( Vx, Vy, X, Y, type )
-% Compute flow at locations X,Y given original flow Vx,Vy.
+function [Vx,Vy] = interpolate( Vx, Vy, X, Y, type )
+% Interpolate flow fields at locations X,Y given flow fields Vx,Vy.
 [h,w]=size(X); XY=@(X,Y) (min(max(X,1),w)-1)*h + min(max(Y,1),h);
-if( type==0 )
-  % just use Vx, Vy (naive accumulation)
-elseif( type==1 )
-  % nearest neighbor interpolation (good accumulation)
+if( type==1 )
   X=round(X); Y=round(Y); Z=XY(X,Y); Vx=Vx(Z); Vy=Vy(Z);
 else
-  % bilinear interpolation (best accumulation)
   Xd0=mod(X,1); Xd1=1-Xd0; Xi=X-Xd0;
   Yd0=mod(Y,1); Yd1=1-Yd0; Yi=Y-Yd0;
   W00=Xd1.*Yd1; W10=Xd0.*Yd1; W01=Xd1.*Yd0; W11=Xd0.*Yd0;
