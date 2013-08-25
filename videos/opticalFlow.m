42ac1f8302c5915823eefe33d4b4baf7196d0464
pdollar <pdollar@gmail.com>
2013-08-25 14:41:16 -0700

    videos/opticalFlow.m: added 'SD' flow type, updated commenting

--- videos/opticalFlow.m
+++ videos/opticalFlow.m
@@ -4,16 +4,22 @@ function [Vx,Vy,reliab] = opticalFlow( I1, I2, varargin )
 % Implemented 'type' of optical flow estimation:
 %  LK: http://en.wikipedia.org/wiki/Lucas-Kanade_method
 %  HS: http://en.wikipedia.org/wiki/Horn-Schunck_method
+%  SD: Simple block-based sum of absolute differences flow
 % LK is a local, fast method (the implementation is fully vectorized).
 % HS is a global, slower method (an SSE implementation is provided).
+% SD is a simple but potentially expensive approach.
 %
-% Common parameters for LK and HS: 'smooth' determines smoothing prior to
-% flow computation and can make flow estimation more robust. 'maxScale' can
-% be used to downsample an image for faster but lower quality results, e.g.
-% maxScale=.5 makes flow computation about 4x faster. LK: 'radius' controls
-% integration window size (and smoothness of flow). HS: 'alpha' controls
-% tradeoff between data and smoothness term (and smoothness of flow) and
-% 'nIter' determines number of gradient decent steps.
+% Common parameters: 'smooth' determines smoothing prior to computing flow
+% and can make flow estimation more robust. 'filt' determines amount of
+% median filtering of the computed flow field which improves results but is
+% costly. 'minScale' and 'maxScale' control image scales in the pyramid.
+% Setting 'maxScale'<1 results in faster but lower quality results, e.g.
+% maxScale=.5 makes flow computation about 4x faster. Method specific
+% parameters: 'radius' controls window size (and smoothness of flow) for LK
+% and SD. 'nBlock' determines number of blocks tested in each direction for
+% SD, computation time is O(nBlock^2). For HS, 'alpha' controls tradeoff
+% between data and smoothness term (and smoothness of flow) and 'nIter'
+% determines number of gradient decent steps.
 %
 % USAGE
 %  [Vx,Vy,reliab] = opticalFlow( I1, I2, pFlow )
@@ -21,18 +27,19 @@ function [Vx,Vy,reliab] = opticalFlow( I1, I2, varargin )
 % INPUTS
 %  I1, I2   - input images to calculate flow between
 %  pFlow    - parameters (struct or name/value pairs)
-%   .type       - ['LK'] may be either 'LK' or 'HS'
+%   .type       - ['LK'] may be 'LK', 'HS' or 'SD'
 %   .smooth     - [1] smoothing radius for triangle filter (may be 0)
 %   .filt       - [0] median filtering radius for smoothing flow field
 %   .minScale   - [1/64] minimum pyramid scale (must be a power of 2)
 %   .maxScale   - [1] maximum pyramid scale (must be a power of 2)
-%   .radius     - [5] integration radius for weighted window [LK only]
+%   .radius     - [10] integration radius for weighted window [LK/SD only]
+%   .nBlock     - [5] number of tested blocks [SD only]
 %   .alpha      - [1] smoothness constraint [HS only]
 %   .nIter      - [250] number of iterations [HS only]
 %
 % OUTPUTS
 %  Vx, Vy   - x,y components of flow  [Vx>0->right, Vy>0->down]
-%  reliab   - reliability of flow in given window [LK only]
+%  reliab   - reliability of flow in given window [LK/SD only]
 %
 % EXAMPLE - compute LK flow on test images
 %  load opticalFlowTest;
@@ -46,26 +53,27 @@ function [Vx,Vy,reliab] = opticalFlow( I1, I2, varargin )
 %  I1=imtransform2(I1,[],'vs',-Vx,'us',-Vy,'pad','replicate');
 %  figure(1); im(I1); figure(2); im(I2);
 %
-% EXAMPLE - compare LK and HS flow
+% EXAMPLE - compare LK/HS/SD flows
 %  load opticalFlowTest;
-%  prm={'smooth',1,'radius',10,'alpha',20,'nIter',200,'type'};
+%  prm={'smooth',1,'radius',10,'alpha',20,'nIter',250,'type'};
 %  tic, [Vx1,Vy1]=opticalFlow(I1,I2,prm{:},'LK'); toc
 %  tic, [Vx2,Vy2]=opticalFlow(I1,I2,prm{:},'HS'); toc
-%  figure(1); im([Vx1 Vy1; Vx2 Vy2]); colormap jet;
+%  tic, [Vx3,Vy3]=opticalFlow(I1,I2,prm{:},'SD','minScale',1); toc
+%  figure(1); im([Vx1 Vy1; Vx2 Vy2; Vx3 Vy3]); colormap jet;
 %
-% See also convTri, imtransform2
+% See also convTri, imtransform2, medfilt2
 %
 % Piotr's Image&Video Toolbox      Version NEW
-% Copyright 2012 Piotr Dollar.  [pdollar-at-caltech.edu]
+% Copyright 2013 Piotr Dollar.  [pdollar-at-caltech.edu]
 % Please email me if you find bugs, or have suggestions or questions!
 % Licensed under the Simplified BSD License [see external/bsd.txt]
 
 % get default parameters and do error checking
 dfs={ 'type','LK', 'smooth',1, 'filt',0, 'minScale',1/64, ...
-  'maxScale',1, 'radius',5, 'alpha',1, 'nIter',250 };
-[type,smooth,filt,minScale,maxScale,radius,alpha,nIter] = ...
+  'maxScale',1, 'radius',10, 'nBlock',5, 'alpha',1, 'nIter',250 };
+[type,smooth,filt,minScale,maxScale,radius,nBlock,alpha,nIter] = ...
   getPrmDflt(varargin,dfs,1);
-assert(any(strcmp(type,{'LK','HS'}))); useLk=strcmp(type,'LK');
+assert(any(strcmp(type,{'LK','HS','SD'})));
 if( ~ismatrix(I1) || ~ismatrix(I2) || any(size(I1)~=size(I2)) )
   error('Input images must be 2D and have same dimensions.'); end
 
@@ -85,8 +93,11 @@ for s=1:max(1,nScales + round(log2(maxScale)))
   % smooth images
   I1s=convTri(I1s,smooth); I2s=convTri(I2s,smooth);
   % run optical flow on current scale
-  if( useLk ), [Vx1,Vy1,reliab]=opticalFlowLk(I1s,I2s,radius);
-  else [Vx1,Vy1]=opticalFlowHs(I1s,I2s,alpha,nIter); reliab=[]; end
+  switch type
+    case 'LK', [Vx1,Vy1,reliab]=opticalFlowLk(I1s,I2s,radius);
+    case 'HS', [Vx1,Vy1]=opticalFlowHs(I1s,I2s,alpha,nIter); reliab=0;
+    case 'SD', [Vx1,Vy1,reliab]=opticalFlowSd(I1s,I2s,radius,nBlock,1);
+  end
   Vx=Vx+Vx1; Vy=Vy+Vy1;
   % finally median filter the resulting flow field
   if(filt), Vx=medfilt2(Vx,[filt filt],'symmetric'); end
@@ -94,6 +105,7 @@ for s=1:max(1,nScales + round(log2(maxScale)))
 end
 r=sqrt(h*w/numel(Vx));
 if(r~=1), Vx=imResample(Vx,[h w])*r; Vy=imResample(Vy,[h w])*r; end
+if(r~=1 && nargout==3), reliab=imResample(reliab,[h w]); end
 
 end
 
@@ -135,6 +147,20 @@ else
     Vx=Vx(2:end-1,2:end-1); Vy=Vy(2:end-1,2:end-1);
   end
 end
+end
+
+function [Vx,Vy,reliab] = opticalFlowSd( I1, I2, radius, nBlock, step )
+% simple block-based sum of absolute differences flow
+[h,w]=size(I1); k=2*nBlock+1; k=k*k; D=zeros(h,w,k,'single'); k=1;
+rng = @(x,w) max(1+x*step,1):min(w+x*step,w);
+for x=-nBlock:nBlock, xs0=rng(x,w); xs1=rng(-x,w);
+  for y=-nBlock:nBlock, ys0=rng(y,h); ys1=rng(-y,h);
+    D(ys0,xs0,k)=abs(I1(ys0,xs0)-I2(ys1,xs1)); k=k+1;
+  end
+end
+D=convTri(D,radius); [reliab,D]=min(D,[],3);
+k=2*nBlock+1; Vy=mod(D-1,k)+1; Vx=(D-Vy)/k+1;
+Vy=(nBlock+1-Vy)*step; Vx=(nBlock+1-Vx)*step;
 end
 
 function J = shift( I, y, x ) %#ok<DEFNU>
