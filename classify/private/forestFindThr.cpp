d7e6d6b5ac160ea00e865c32756abba3c3f884bc
pdollar <pdollar@gmail.com>
2013-09-29 17:13:30 -0700

    classify/private/forestFindThr.cpp: major cleanup (variable renaming, etc)

--- classify/private/forestFindThr.cpp
+++ classify/private/forestFindThr.cpp
@@ -8,34 +8,39 @@
 #include <mex.h>
 typedef unsigned int uint32;
 
-void forestFindThr( int H, int N, int F, const float *data, const uint32 *hs,
-  const float *ws, const uint32 *order, uint32 &fid, float &thr, double &gini )
+inline double gini( double p ) { return p*p; }
+
+void forestFindThr( int H, int N, int F, const float *data,
+  const uint32 *hs, const float *ws, const uint32 *order,
+  uint32 &fid, float &thr, double &impurity )
 {
-  int i, j, h; double *L, *R, *T; float *data1; uint32 *order1;
-  double gini1, s=0, sl, sr, g=0, gl, gr;
-  L=new double[H]; R=new double[H]; T=new double[H];
-  for( i=0; i<H; i++ ) T[i] = 0;
-  for( j=0; j<N; j++ ) { s+=ws[j]; T[hs[j]-1]+=ws[j]; }
-  for( i=0; i<H; i++ ) g+=T[i]*T[i];
-  fid = 1; thr = 0; gini = 1e5;
+  int i, j, j1, j2, h; double *Wl, *Wr, *W; float *data1; uint32 *order1;
+  double impurity1, w=0, wl, wr, g=0, gl, gr;
+  Wl=new double[H]; Wr=new double[H]; W=new double[H];
+  for( i=0; i<H; i++ ) W[i] = 0;
+  for( j=0; j<N; j++ ) { w+=ws[j]; W[hs[j]-1]+=ws[j]; }
+  for( i=0; i<H; i++ ) g += gini(W[i]);
+  fid = 1; thr = 0; impurity = 1e5;
   for( i=0; i<F; i++ ) {
     order1=(uint32*) order+i*N; data1=(float*) data+i*size_t(N);
-    for( j=0; j<H; j++ ) { L[j]=0; R[j]=T[j]; } gl=sl=0; gr=g;
+    for( j=0; j<H; j++ ) { Wl[j]=0; Wr[j]=W[j]; } gl=wl=0; gr=g; wr=w;
     for( j=0; j<N-1; j++ ) {
-      int j1=order1[j], j2=order1[j+1]; h=hs[j1]-1; sl+=ws[j1]; sr=s-sl;
-      gl-=L[h]*L[h]; L[h]+=ws[j1]; gl+=L[h]*L[h];
-      gr-=R[h]*R[h]; R[h]-=ws[j1]; gr+=R[h]*R[h];
-      if( data1[j2]-data1[j1]<1e-6f ) continue;
-      gini1 = (sl-gl/sl)/s + (sr-gr/sr)/s; // + (sl*sl+sr*sr)/(s*s*100);
-      if(gini1<gini) { gini=gini1; fid=i+1; thr=0.5f*(data1[j1]+data1[j2]); }
+      // gini=1-\sum_h p_h^2; impurity=ginil*wl/w+ginir*wr/w
+      j1=order1[j]; j2=order1[j+1]; h=hs[j1]-1;
+      wl+=ws[j1]; gl-=gini(Wl[h]); Wl[h]+=ws[j1]; gl+=gini(Wl[h]);
+      wr-=ws[j1]; gr-=gini(Wr[h]); Wr[h]-=ws[j1]; gr+=gini(Wr[h]);
+      impurity1 = (wl-gl/wl)/w + (wr-gr/wr)/w;
+      if( impurity1<impurity && data1[j2]-data1[j1]>=1e-6f ) {
+        impurity=impurity1; fid=i+1; thr=0.5f*(data1[j1]+data1[j2]); }
     }
   }
-  delete [] L; delete [] R; delete [] T;
+  delete [] Wl; delete [] Wr; delete [] W;
 }
 
-// [fid,thr,gini] = mexFunction(data,hs,ws,order,H);
+// [fid,thr,impurity] = mexFunction(data,hs,ws,order,H);
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
-  int H, N, F; float *data, *ws, thr; double gini; uint32 *hs, *order, fid;
+  int H, N, F; float *data, *ws, thr;
+  double impurity; uint32 *hs, *order, fid;
   data = (float*) mxGetData(prhs[0]);
   hs = (uint32*) mxGetData(prhs[1]);
   ws = (float*) mxGetData(prhs[2]);
@@ -43,8 +48,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   H = (int) mxGetScalar(prhs[4]);
   N = (int) mxGetM(prhs[0]);
   F = (int) mxGetN(prhs[0]);
-  forestFindThr(H,N,F,data,hs,ws,order,fid,thr,gini);
+  forestFindThr(H,N,F,data,hs,ws,order,fid,thr,impurity);
   plhs[0] = mxCreateDoubleScalar(fid);
   plhs[1] = mxCreateDoubleScalar(thr);
-  plhs[2] = mxCreateDoubleScalar(gini);
+  plhs[2] = mxCreateDoubleScalar(impurity);
 }
